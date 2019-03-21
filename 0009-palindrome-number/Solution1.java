8ba9b6e5a25186519ddf9a16f64b505944fc59fa
Frank Zou <frankbearzou@gmail.com>
2019-03-21 14:44:54 -0400

    Create Solution1.java

---
+++ 0009-palindrome-number/Solution1.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        if (x < 0) return false;
+        String s = Integer.toString(x);
+        int len = s.length();
+        int round = s.length() / 2;
+        int i = 0;
+        while (i < round) {
+            if (s.charAt(i) != s.charAt(len - i - 1)) return false;
+            ++i;
+        }
+        return true;
+    }
+}
