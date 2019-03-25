364a7597e44bbf44b91a96eb19a81d78c4821bd2
Frank Zou <frankbearzou@gmail.com>
2019-03-25 10:43:49 -0400

    Create Solution.java

---
+++ 0035-search-insert-position/Solution.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        int i = 0;
+        while (i < nums.length) {
+            if (target <= nums[i])
+                return i;
+            ++i;
+        }
+        return i;
+    }
+}
