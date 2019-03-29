b36cc5c92b46511e67dc627a2c330a659b72f67d
Frank Zou <frankbearzou@gmail.com>
2019-03-29 14:43:51 -0400

    Create Solution.java

---
+++ 0961-n-repeated-element-in-size-2n-array/Solution.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int repeatedNTimes(int[] A) {
+        Set<Integer> set = new HashSet<>();
+        for (int i : A) {
+            if (set.contains(i))
+                return i;
+            set.add(i);
+        }
+        return 0;
+    }
+}
