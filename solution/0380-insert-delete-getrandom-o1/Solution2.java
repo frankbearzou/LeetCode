430d02e1842d6329dc3b39787b6b3af454bf1997
Frank Zou <frankbearzou@gmail.com>
2020-04-07 17:40:07 -0400

    Create Solution2.java

---
+++ solution/0380-insert-delete-getrandom-o1/Solution2.java
@@ -0,0 +1,36 @@
+class RandomizedSet {
+    private Set<Integer> set;
+    /** Initialize your data structure here. */
+    public RandomizedSet() {
+        set = new HashSet<>();
+    }
+    
+    /** Inserts a value to the set. Returns true if the set did not already contain the specified element. */
+    public boolean insert(int val) {
+        return set.add(val);
+    }
+    
+    /** Removes a value from the set. Returns true if the set contained the specified element. */
+    public boolean remove(int val) {
+        return set.remove(val);
+    }
+    
+    /** Get a random element from the set. */
+    public int getRandom() {
+        Random r = new Random();
+        int next = Math.min(r.nextInt(set.size()), 10);
+        Iterator<Integer> iter = set.iterator();
+        for (int i = 0; i < next; i++) {
+            iter.next();
+        }
+        return iter.next();
+    }
+}
+
+/**
+ * Your RandomizedSet object will be instantiated and called as such:
+ * RandomizedSet obj = new RandomizedSet();
+ * boolean param_1 = obj.insert(val);
+ * boolean param_2 = obj.remove(val);
+ * int param_3 = obj.getRandom();
+ */
