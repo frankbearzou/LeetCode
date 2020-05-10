1aa621510f6e1e2464cbf349f7ed8271ee3bbe43
Frank Zou <frankbearzou@gmail.com>
2020-05-10 17:16:48 -0400

    Create Solution.java

---
+++ solution/0701-insert-into-a-binary-search-tree/Solution.java
@@ -0,0 +1,43 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    private void insert(TreeNode root, int val) {
+        if (root == null)
+            return;
+        if (root.val > val) {
+            if (root.left == null) {
+                root.left = new TreeNode(val);
+                return;
+            } else {
+                insert(root.left, val);
+            }
+        } else if (root.val < val) {
+            if (root.right == null) {
+                root.right = new TreeNode(val);
+                return;
+            } else {
+                insert(root.right, val);
+            }
+        }
+    }
+    
+    public TreeNode insertIntoBST(TreeNode root, int val) {
+        if (root == null)
+            return new TreeNode(val);
+        insert(root, val);
+        return root;
+    }
+}
