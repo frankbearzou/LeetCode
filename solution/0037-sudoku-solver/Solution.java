fb543da7c8e387829b441421e8d68c8fb3ae1c53
Frank Zou <frankbearzou@gmail.com>
2020-05-21 15:24:22 -0400

    Create Solution.java

---
+++ solution/0037-sudoku-solver/Solution.java
@@ -0,0 +1,38 @@
+class Solution {
+    public void solveSudoku(char[][] board) {
+        solve(board);
+    }
+
+    private boolean solve(char[][] board) {
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[0].length; j++) {
+                if (board[i][j] == '.') {
+                    for (char ch = '1'; ch <= '9'; ch++) {
+                        if (valid(board, i, j, ch)) {
+                            board[i][j] = ch;
+                            if (solve(board))
+                                return true;
+                            else
+                                board[i][j] = '.';
+                        }
+                    }
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    private boolean valid(char[][] board, int row, int col, char ch) {
+        for (int i = 0; i < 9; i++) {
+            if (board[row][i] == ch)
+                return false;
+            if (board[i][col] == ch)
+                return false;
+            if (board[(row / 3) * 3 + i / 3][(col / 3) * 3 + i % 3] == ch)
+                return false;
+        }
+        
+        return true;
+    }
+}
