600b2f67299d8015fb3eb44194f9fdbac8ba65e4
Frank Zou <frankbearzou@gmail.com>
2020-06-28 12:18:42 -0400

    Create Solution.java

---
+++ solution/1048-running-sum-of-1d-array/Solution.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int[] runningSum(int[] nums) {
+        if (nums.length < 2)
+            return nums;
+        
+        int[] ans = new int[nums.length];
+        ans[0] = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            ans[i] = ans[i - 1] + nums[i];
+        }
+        
+        return ans;
+    }
+}
