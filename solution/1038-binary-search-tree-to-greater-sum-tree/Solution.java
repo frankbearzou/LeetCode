cfb10437c3313bd254cb0ef9eefde0be869e6b09
Frank Zou <frankbearzou@gmail.com>
2020-07-16 20:43:38 -0400

    Create Solution.java

---
+++ solution/1038-binary-search-tree-to-greater-sum-tree/Solution.java
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    private int total = 0;
+    
+    public TreeNode bstToGst(TreeNode root) {
+        if (root.right != null)
+            bstToGst(root.right);
+        root.val += total;
+        total = root.val;
+        if (root.left != null)
+            bstToGst(root.left);
+        return root;
+    }
+}
