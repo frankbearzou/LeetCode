71c839fcbd3e170d420a43a6daecd1b2acece1c9
Frank Zou <frankbearzou@gmail.com>
2020-08-04 22:23:41 -0400

    Create Solution.java

---
+++ solution/0342-power-of-four/Solution.java
@@ -0,0 +1,10 @@
+class Solution {
+    public boolean isPowerOfFour(int num) {
+        while (num > 1) {
+            if (num % 4 != 0)
+                return false;
+            num /= 4;
+        }
+        return num == 1;
+    }
+}
