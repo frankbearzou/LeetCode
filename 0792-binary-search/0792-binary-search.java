4d88dfb8fa9306ea7d5d37f2123d7e6c56d3a2bb
Frank Zou <frankbearzou@gmail.com>
2023-10-20 15:56:02 -0400

    Time: 0 ms (100%), Space: 44.7 MB (59.84%) - LeetHub

---
+++ 0792-binary-search/0792-binary-search.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        int l = 0, r = nums.length - 1, m = l + (r - l) / 2;
+        while (l <= r) {
+            m = l + (r - l) / 2;
+            if (nums[m] == target) {
+                return m;
+            } else if (nums[m] < target) {
+                l = m + 1;
+            } else {
+                r = m - 1;
+            }
+        }
+        return -1;
+    }
+}
