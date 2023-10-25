f4237ce9b170434a4d628113bf25b18bf45a7b30
Frank Zou <frankbearzou@gmail.com>
2023-10-25 11:55:54 -0400

    Time: 2 ms (56.11%), Space: 34.5 MB (100%) - LeetHub

---
+++ 0067-add-binary/0067-add-binary.java
@@ -0,0 +1,18 @@
+class Solution {
+    public String addBinary(String a, String b) {
+        int i = a.length() - 1;
+        int j = b.length() - 1;
+        int carry = 0;
+        StringBuilder sb = new StringBuilder();
+        
+        while (i >= 0 || j >= 0) {
+            int sum = carry;
+            if (i >= 0) sum += a.charAt(i--) - '0';
+            if (j >= 0) sum += b.charAt(j--) - '0';
+            sb.append(sum & 1);
+            carry = sum >> 1;
+        }
+        if (carry == 1) sb.append(1);
+        return sb.reverse().toString();
+    }
+}
