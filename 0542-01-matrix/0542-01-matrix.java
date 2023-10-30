a0f4ecc7a18f3fdfe3b8458e3dc28c07dc93fea6
Frank Zou <frankbearzou@gmail.com>
2023-10-30 10:05:06 -0400

    Time: 6 ms (99.48%), Space: 45 MB (95.14%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.java
+++ 0542-01-matrix/0542-01-matrix.java
@@ -1,24 +1,35 @@
 class Solution {
     public int[][] updateMatrix(int[][] mat) {
-        int row = mat.length, column = mat[0].length;
-        Queue<int[]> queue = new LinkedList<>();
-        for (int i = 0; i < row; i++) {
-            for (int j = 0; j < column; j++) {
-                if (mat[i][j] == 0) {
-                    queue.offer(new int[]{i, j});
-                } else {
-                    mat[i][j] = row * column;
+        for (int i = 0; i < mat.length; i++) {
+            for (int j = 0; j < mat[0].length; j++) {
+                if (mat[i][j] == 1) {
+                    mat[i][j] = mat.length + mat[0].length;
                 }
             }
         }
-        int[][] dir = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
-        while (!queue.isEmpty()) {
-            int[] q = queue.poll();
-            for (int[] d : dir) {
-                int r = q[0] + d[0], c = q[1] + d[1];
-                if (r >= 0 && r < row && c >= 0 && c < column && mat[r][c] > mat[q[0]][q[1]] + 1) {
-                    queue.offer(new int[]{r, c});
-                    mat[r][c] = mat[q[0]][q[1]] + 1;
+        // from top left to bottom down
+        for (int i = 0; i < mat.length; i++) {
+            for (int j = 0; j < mat[0].length; j++) {
+                if (mat[i][j] == 0)
+                    continue;
+                if (i > 0) {
+                    mat[i][j] = Math.min(mat[i][j], mat[i - 1][j] + 1);
+                }
+                if (j > 0) {
+                    mat[i][j] = Math.min(mat[i][j], mat[i][j - 1] + 1);
+                }
+            }
+        }
+        // from botton down to top left
+        for (int i = mat.length - 1; i >= 0; i--) {
+            for (int j = mat[0].length - 1; j >= 0; j--) {
+                if (mat[i][j] == 0)
+                    continue;
+                if (i < mat.length - 1) {
+                    mat[i][j] = Math.min(mat[i][j], mat[i + 1][j] + 1);
+                }
+                if (j < mat[0].length - 1) {
+                    mat[i][j] = Math.min(mat[i][j], mat[i][j + 1] + 1);
                 }
             }
         }
