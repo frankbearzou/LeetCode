c7cb7d57017e6d8fbfa81e44f74907421928431b
Frank Zou <frankbearzou@gmail.com>
2023-11-09 16:18:27 -0500

    Time: 21 ms (87.93%), Space: 57.1 MB (14.17%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
@@ -1,23 +1,24 @@
 class Solution {
+    Boolean[][] dp;
     public boolean canPartition(int[] nums) {
         int sum = 0;
-        for (int i : nums) {
-            sum += i;
+        for (int n : nums) {
+            sum += n;
         }
         if (sum % 2 == 1)
             return false;
-        int target = sum / 2;
-        Set<Integer> dp = new HashSet<>();
-        dp.add(0);
-        for (int i : nums) {
-            Set<Integer> set = new HashSet<>(dp);
-            for (int v : dp) {
-                if (v + i == target)
-                    return true;
-                set.add(v + i);
-            }
-            dp = set;
-        }
-        return false;
+        dp = new Boolean[nums.length + 1][sum / 2 + 1];
+        return canPartition(nums, 0, sum / 2);
+    }
+
+    boolean canPartition(int[] nums, int index, int target) {
+        if (target == 0)
+            return true;
+        if (target < 0 || index >= nums.length)
+            return false;
+        if (dp[index][target] != null)
+            return dp[index][target];
+        dp[index][target] = canPartition(nums, index + 1, target - nums[index]) || canPartition(nums, index + 1, target);
+        return dp[index][target];
     }
 }
