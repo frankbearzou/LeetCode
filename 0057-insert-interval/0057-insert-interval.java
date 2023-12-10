d01beba36357e213f0faa39087f7c623e13729c5
Frank Zou <frankbearzou@gmail.com>
2023-12-10 17:41:39 -0500

    Time: 2 ms (48.5%), Space: 43.4 MB (95.44%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.java
+++ 0057-insert-interval/0057-insert-interval.java
@@ -1,28 +1,30 @@
 class Solution {
     public int[][] insert(int[][] intervals, int[] newInterval) {
-        List<int[]> res = new ArrayList<>();
+        List<int[]> list = new ArrayList<>();
         for (int i = 0; i < intervals.length; i++) {
             if (newInterval[1] < intervals[i][0]) {
-                res.add(newInterval);
+                list.add(newInterval);
                 for (int j = i; j < intervals.length; j++) {
-                    res.add(intervals[j]);
+                    list.add(intervals[j]);
                 }
-                int[][] ans = new int[res.size()][2];
+                int size = list.size();
+                int[][] ans = new int[size][2];
                 for (int j = 0; j < ans.length; j++) {
-                    ans[j] = res.get(j);
+                    ans[j] = list.get(j);
                 }
                 return ans;
-            } else if (newInterval[0] > intervals[i][1]) {
-                res.add(intervals[i]);
+            } else if (intervals[i][1] < newInterval[0]) {
+                list.add(intervals[i]);
             } else {
-                newInterval[0] = Math.min(intervals[i][0], newInterval[0]);
-                newInterval[1] = Math.max(intervals[i][1], newInterval[1]);
+                newInterval[0] = Math.min(newInterval[0], intervals[i][0]);
+                newInterval[1] = Math.max(newInterval[1], intervals[i][1]);
             }
         }
-        res.add(newInterval);
-        int[][] ans = new int[res.size()][2];
-        for (int i = 0; i < ans.length; i++) {
-            ans[i] = res.get(i);
+        list.add(newInterval);
+        int size = list.size();
+        int[][] ans = new int[size][2];
+        for (int j = 0; j < ans.length; j++) {
+            ans[j] = list.get(j);
         }
         return ans;
     }
