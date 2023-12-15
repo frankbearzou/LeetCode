aef92eb830056c47fd5a1892f2ec5bfc7eb2996a
Frank Zou <frankbearzou@gmail.com>
2023-12-15 14:16:53 -0500

    Time: 3 ms (75.63%), Space: 43.8 MB (71.14%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.java
+++ 0383-ransom-note/0383-ransom-note.java
@@ -1,16 +1,12 @@
 class Solution {
     public boolean canConstruct(String ransomNote, String magazine) {
         int[] map = new int[26];
-        for (char c : ransomNote.toCharArray()) {
-            map[(int)(c - 'a')]++;
+        for (int i = 0; i < magazine.length(); i++) {
+            map[magazine.charAt(i) - 'a']++;
         }
-        for (char c : magazine.toCharArray()) {
-            map[(int)(c - 'a')]--;
-        }
-        for (int i = 0; i < map.length; i++) {
-            if (map[i] > 0) {
+        for (int i = 0; i < ransomNote.length(); i++) {
+            if (--map[ransomNote.charAt(i) - 'a'] < 0)
                 return false;
-            }
         }
         return true;
     }
