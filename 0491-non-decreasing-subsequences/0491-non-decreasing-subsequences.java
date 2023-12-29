cbce2eed9b99461ee6e881e8198591dbccf588d9
Frank Zou <frankbearzou@gmail.com>
2023-12-29 16:41:35 -0500

    Time: 13 ms (36.65%), Space: 50.9 MB (35.59%) - LeetHub

--- 0491-non-decreasing-subsequences/0491-non-decreasing-subsequences.java
+++ 0491-non-decreasing-subsequences/0491-non-decreasing-subsequences.java
@@ -1,20 +1,22 @@
 class Solution {
     public List<List<Integer>> findSubsequences(int[] nums) {
-        Set<List<Integer>> ans = new HashSet<>();
+        List<List<Integer>> ans = new ArrayList<>();
         List<Integer> path = new ArrayList<>();
         backtracking(nums, 0, path, ans);
         return new ArrayList(ans);
     }
 
-    void backtracking(int[] nums, int start, List<Integer> path, Set<List<Integer>> ans) {
+    void backtracking(int[] nums, int start, List<Integer> path, List<List<Integer>> ans) {
         if (path.size() >= 2) {
             ans.add(new ArrayList(path));
         }
+        Set<Integer> used = new HashSet<>();
         for (int i = start; i < nums.length; i++) {
-            if (i > start && nums[i] == nums[i - 1])
-                continue;
             if (!path.isEmpty() && nums[i] < path.get(path.size() - 1))
                 continue;
+            if (used.contains(nums[i]))
+                continue;
+            used.add(nums[i]);
             path.add(nums[i]);
             backtracking(nums, i + 1, path, ans);
             path.remove(path.size() - 1);
