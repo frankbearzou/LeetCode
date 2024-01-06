e5cda43d72883e27403c3ebd671fa4516e037fbe
Frank Zou <frankbearzou@gmail.com>
2024-01-06 14:12:43 -0500

    Time: 0 ms (100%), Space: 40.1 MB (22.61%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,12 +1,17 @@
 class Solution {
+    Map<Integer, Integer> cache = new HashMap<>();
+
     public int climbStairs(int n) {
-        // a, b, 2, 3, ... n
-        int a = 1, b = 1;
-        for (int i = 1; i < n; i++) {
-            int t = a + b;
-            a = b;
-            b = t;
-        }
-        return b;
+        return dfs(n);
+    }
+
+    int dfs(int n) {
+        if (n <= 2)
+            return n;
+        if (cache.containsKey(n))
+            return cache.get(n);
+        int ret = dfs(n - 1) + dfs(n - 2);
+        cache.put(n, ret);
+        return ret;
     }
 }
