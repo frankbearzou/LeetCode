18e2af7cd0a20a3c62e104c3e767089f5278daae
Frank Zou <frankbearzou@gmail.com>
2024-01-06 11:07:09 -0500

    Time: 0 ms (100%), Space: 40.4 MB (13.9%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -1,16 +1,13 @@
 class Solution {
     public int fib(int n) {
-        Map<Integer, Integer> cache = new HashMap<>();
-        return fib(n, cache);
-    }
-
-    int fib(int n, Map<Integer, Integer> cache) {
-        if (n <= 1)
+        if (n < 2)
             return n;
-        if (cache.containsKey(n))
-            return cache.get(n);
-        int ret = fib(n - 1) + fib(n - 2);
-        cache.put(n, ret);
-        return ret;
+        int[] dp = new int[n + 1];
+        dp[0] = 0;
+        dp[1] = 1;
+        for (int i = 2; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+        return dp[n];
     }
 }
