12f2ff31a22ddcd783134a561fe10734f9ac8afe
Frank Zou <frankbearzou@gmail.com>
2024-01-06 19:12:03 -0500

    Time: 0 ms (100%), Space: 42.2 MB (5.03%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.java
+++ 0063-unique-paths-ii/0063-unique-paths-ii.java
@@ -1,39 +1,26 @@
 class Solution {
     public int uniquePathsWithObstacles(int[][] obstacleGrid) {
-        if (obstacleGrid[0][0] == 1)
+        int r = obstacleGrid.length, c = obstacleGrid[0].length;
+        if (obstacleGrid[0][0] == 1 || obstacleGrid[r - 1][c - 1] == 1)
             return 0;
-        for (int i = 0; i < obstacleGrid.length; i++) {
-            for (int j = 0; j < obstacleGrid[0].length; j++) {
+        int[][] dp = new int[r][c];
+        for (int i = 0; i < r; i++) {
+            if (obstacleGrid[i][0] == 1)
+                break;
+            dp[i][0] = 1;
+        }
+        for (int i = 0; i < c; i++) {
+            if (obstacleGrid[0][i] == 1)
+                break;
+            dp[0][i] = 1;
+        }
+        for (int i = 1; i < r; i++) {
+            for (int j = 1; j < c; j++) {
                 if (obstacleGrid[i][j] == 1)
-                    obstacleGrid[i][j] = -1;
-            }
-        }
-        for (int i = 0; i < obstacleGrid.length; i++) {
-            if (obstacleGrid[i][0] == -1)
-                break;
-            obstacleGrid[i][0] = 1;
-        }
-        for (int i = 0; i < obstacleGrid[0].length; i++) {
-            if (obstacleGrid[0][i] == -1)
-                break;
-            obstacleGrid[0][i] = 1;
-        }
-        for (int i = 1; i < obstacleGrid.length; i++) {
-            for (int j = 1; j < obstacleGrid[0].length; j++) {
-                if (obstacleGrid[i][j] == -1)
                     continue;
-                int left = obstacleGrid[i][j - 1];
-                if (left == -1)
-                    left = 0;
-                int up = obstacleGrid[i - 1][j];
-                if (up == -1)
-                    up = 0;
-                obstacleGrid[i][j] = left + up;
+                dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
             }
         }
-        int ans = obstacleGrid[obstacleGrid.length - 1][obstacleGrid[0].length - 1];
-        if (ans == -1)
-            return 0;
-        return ans;
+        return dp[r - 1][c - 1];
     }
 }
