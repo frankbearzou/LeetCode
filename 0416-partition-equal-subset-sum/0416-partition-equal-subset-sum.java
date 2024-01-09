fa692394b850a55198ab8a784c775902097176d5
Frank Zou <frankbearzou@gmail.com>
2024-01-09 18:28:10 -0500

    Time: 49 ms (48.96%), Space: 57.3 MB (22.2%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
@@ -1,24 +1,25 @@
 class Solution {
-    Boolean[][] dp;
     public boolean canPartition(int[] nums) {
         int sum = 0;
-        for (int n : nums) {
-            sum += n;
+        for (int num : nums) {
+            sum += num;
         }
-        if (sum % 2 == 1)
+        if (sum % 2 != 0)
             return false;
-        dp = new Boolean[nums.length + 1][sum / 2 + 1];
-        return canPartition(nums, 0, sum / 2);
+        Boolean[][] cache = new Boolean[nums.length][sum / 2 + 1];
+        return dfs(nums, 0, sum / 2, cache);
     }
 
-    boolean canPartition(int[] nums, int index, int target) {
+    boolean dfs(int[] nums, int index, int target, Boolean[][] cache) {
+        if (index >= nums.length)
+            return false;
+        if (target < 0)
+            return false;
         if (target == 0)
             return true;
-        if (target < 0 || index >= nums.length)
-            return false;
-        if (dp[index][target] != null)
-            return dp[index][target];
-        dp[index][target] = canPartition(nums, index + 1, target - nums[index]) || canPartition(nums, index + 1, target);
-        return dp[index][target];
+        if (cache[index][target] != null)
+            return cache[index][target];
+        cache[index][target] = dfs(nums, index + 1, target, cache) || dfs(nums, index + 1, target - nums[index], cache);
+        return cache[index][target];
     }
 }
