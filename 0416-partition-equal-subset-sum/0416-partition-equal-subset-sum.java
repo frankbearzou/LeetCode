34019762c47554f45a0df12bea33d6912fbc749f
Frank Zou <frankbearzou@gmail.com>
2024-01-11 11:17:01 -0500

    Time: 22 ms (86.83%), Space: 42.2 MB (84.4%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
@@ -7,13 +7,12 @@ class Solution {
         if ((sum & 1) == 1)
             return false;
         int target = sum / 2;
-        boolean[] dp = new boolean[target + 1];
-        dp[0] = true;
+        int[] dp = new int[target + 1];
         for (int i = 0; i < nums.length; i++) {
             for (int j = target; j >= nums[i]; j--) {
-                dp[j] = dp[j] || dp[j - nums[i]];
+                dp[j] = Math.max(dp[j], dp[j - nums[i]] + nums[i]);
             }
         }
-        return dp[target];
+        return dp[target] == target;
     }
 }
