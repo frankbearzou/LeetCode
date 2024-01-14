8c106d766582fa58f369ab71cb09f6d878757bbd
Frank Zou <frankbearzou@gmail.com>
2024-01-14 16:53:44 -0500

    Time: 467 ms (7.65%), Space: 121.5 MB (5.1%) - LeetHub

--- 0474-ones-and-zeroes/0474-ones-and-zeroes.java
+++ 0474-ones-and-zeroes/0474-ones-and-zeroes.java
@@ -1,21 +1,30 @@
 class Solution {
     public int findMaxForm(String[] strs, int m, int n) {
-        int[][] dp = new int[m + 1][n + 1];
-        for (String str : strs) {
-            int zero = 0, one = 0;
-            for (char c : str.toCharArray()) {
-                if (c == '0') {
-                    zero++;
-                } else {
-                    one++;
-                }
-            }
-            for (int i = m; i >= zero; i--) {
-                for (int j = n; j >= one; j--) {
-                    dp[i][j] = Math.max(dp[i][j], dp[i - zero][j - one] + 1);
-                }
+        Integer[][][] cache = new Integer[601][101][101];
+        return dfs(strs, 0, m, n, cache);
+    }
+
+    int dfs(String[] strs, int index, int zeros, int ones, Integer[][][] cache) {
+        if (index >= strs.length)
+            return 0;
+        if (zeros == 0 && ones == 0)
+            return 0;
+        if (cache[index][zeros][ones] != null)
+            return cache[index][zeros][ones];
+        String str = strs[index];
+        int zero = 0, one = 0;
+        for (char c : str.toCharArray()) {
+            if (c == '0') {
+                zero++;
+            } else {
+                one++;
             }
         }
-        return dp[m][n];
+        if (zero > zeros || one > ones)
+            return dfs(strs, index + 1, zeros, ones, cache);
+        int no = dfs(strs, index + 1, zeros, ones, cache);
+        int yes = dfs(strs, index + 1, zeros - zero, ones - one, cache) + 1;
+        cache[index][zeros][ones] = Math.max(no, yes);
+        return cache[index][zeros][ones];
     }
 }
