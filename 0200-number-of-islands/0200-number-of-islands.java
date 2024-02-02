582e036be71f4db28ff28c21b5919408ff8e89a6
Frank Zou <frankbearzou@gmail.com>
2024-02-02 10:09:12 -0500

    Time: 7 ms (20.08%), Space: 49.9 MB (25.7%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -1,26 +1,35 @@
 class Solution {
     public int numIslands(char[][] grid) {
         int ans = 0;
+        boolean[][] visited = new boolean[grid.length][grid[0].length];
         for (int i = 0; i < grid.length; i++) {
             for (int j = 0; j < grid[0].length; j++) {
-                if (grid[i][j] == '1') {
+                if (!visited[i][j] && grid[i][j] == '1') {
                     ans++;
-                    dfs(grid, i, j);
+                    bfs(grid, i, j, visited);
                 }
             }
         }
         return ans;
     }
 
-    void dfs(char[][] grid, int row, int col) {
-        if (row < 0 || row >= grid.length || col < 0 || col >= grid[0].length)
-            return;
-        if (grid[row][col] == '0')
-            return;
-        grid[row][col] = '0';
-        dfs(grid, row + 1, col);
-        dfs(grid, row - 1, col);
-        dfs(grid, row, col + 1);
-        dfs(grid, row, col - 1);
+    void bfs(char[][] grid, int row, int col, boolean[][] visited) {
+        Queue<int[]> queue = new LinkedList<>();
+        queue.offer(new int[]{row, col});
+        visited[row][col] = true;
+        while (!queue.isEmpty()) {
+            int[] poll = queue.poll();
+            int[][] dirs = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+            for (int[] dir : dirs) {
+                int newx = poll[0] + dir[0];
+                int newy = poll[1] + dir[1];
+                if (newx < 0 || newx >= grid.length || newy < 0 || newy >= grid[0].length)
+                    continue;
+                if (!visited[newx][newy] && grid[newx][newy] == '1') {
+                    queue.offer(new int[]{newx, newy});
+                    visited[newx][newy] = true;
+                }
+            }
+        }
     }
 }
