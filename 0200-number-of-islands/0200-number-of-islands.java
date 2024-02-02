27fb55e1d0fc95eca5bb1cc12f86d5f965941daf
Frank Zou <frankbearzou@gmail.com>
2024-02-02 09:43:31 -0500

    Time: 3 ms (86.04%), Space: 49.3 MB (55.24%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -1,37 +1,26 @@
 class Solution {
     public int numIslands(char[][] grid) {
-        int ans = 0, row = grid.length, column = grid[0].length;
-        boolean[][] visited = new boolean[row][column];
-        for (int i = 0; i < row; i++) {
-            for (int j = 0; j < column; j++) {
-                if (grid[i][j] == '1')
-                    grid[i][j] = '2';
-            }
-        }
-        for (int i = 0; i < row; i++) {
-            for (int j = 0; j < column; j++) {
-                if (grid[i][j] == '2' && !visited[i][j]) {
+        int ans = 0;
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                if (grid[i][j] == '1') {
                     ans++;
-                    dfs(grid, i, j, visited);
+                    dfs(grid, i, j);
                 }
             }
         }
         return ans;
     }
 
-    private void dfs(char[][] grid, int r, int c, boolean[][] visited) {
-        int row = grid.length, column = grid[0].length;
-        if (r < 0 || r >= row || c < 0 || c >= column)
+    void dfs(char[][] grid, int row, int col) {
+        if (row < 0 || row >= grid.length || col < 0 || col >= grid[0].length)
             return;
-        if (grid[r][c] != '2')
+        if (grid[row][col] == '0')
             return;
-        if (visited[r][c])
-            return;
-        grid[r][c] = '1';
-        int[][] directions = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
-        for (int[] dir : directions) {
-            int newR = r + dir[0], newC = c + dir[1];
-            dfs(grid, newR, newC, visited);
-        }
+        grid[row][col] = '0';
+        dfs(grid, row + 1, col);
+        dfs(grid, row - 1, col);
+        dfs(grid, row, col + 1);
+        dfs(grid, row, col - 1);
     }
 }
