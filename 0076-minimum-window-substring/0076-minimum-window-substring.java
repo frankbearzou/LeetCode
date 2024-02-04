3cc5b5f59f4955a120d7e17b2a187949dd49ccf0
Frank Zou <frankbearzou@gmail.com>
2024-02-04 12:17:45 -0500

    Time: 52 ms (28.4%), Space: 45.4 MB (20.7%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.java
+++ 0076-minimum-window-substring/0076-minimum-window-substring.java
@@ -1,29 +1,34 @@
 class Solution {
     public String minWindow(String s, String t) {
-        int l = 0, r = 0, minLen = Integer.MAX_VALUE, count = t.length();
+        if (t.length() > s.length())
+            return "";
         String ans = "";
+        int minLen = Integer.MAX_VALUE;
         int[] map = new int[128];
-
-        for (int c : t.toCharArray()) {
+        for (char c : t.toCharArray()) {
             map[c]++;
         }
-
-        char[] chs = s.toCharArray();
-        while (r < chs.length) {
-            if (map[chs[r++]]-- > 0) {
+        int count = t.length();
+        int l = 0, r = 0;
+        char[] arr = s.toCharArray();
+        while (r < arr.length) {
+            if (map[arr[r]] > 0) {
                 count--;
             }
+            map[arr[r]]--;
+            r++;
             while (count == 0) {
                 if (r - l < minLen) {
                     minLen = r - l;
                     ans = s.substring(l, r);
                 }
-                if (map[chs[l++]]++ == 0) {
+                if (map[arr[l]] == 0) {
                     count++;
                 }
+                map[arr[l]]++;
+                l++;
             }
         }
-
         return ans;
     }
 }
