4f8fd50147d5139c068fedad848d011c0c15e8dc
Frank Zou <frankbearzou@gmail.com>
2024-02-09 23:09:58 -0500

    Time: 6 ms (45.89%), Space: 45.3 MB (12.22%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.java
+++ 0647-palindromic-substrings/0647-palindromic-substrings.java
@@ -1,20 +1,22 @@
 class Solution {
     public int countSubstrings(String s) {
-        int ans = 0;
-        boolean[][] dp = new boolean[s.length()][s.length()];
-        for (int i = s.length() - 1; i >= 0; i--) {
-            for (int j = i; j < s.length(); j++) {
-                if (s.charAt(i) == s.charAt(j)) {
-                    if (j - i <= 1) {
-                        ans++;
+        int count = 0;
+        int N = s.length();
+        char[] arr = s.toCharArray();
+        boolean[][] dp = new boolean[N][N];
+        for (int i = N - 1; i >= 0; i--) {
+            for (int j = i; j < N; j++) {
+                if (arr[i] == arr[j]) {
+                    if (i == j || i + 1 == j) {
                         dp[i][j] = true;
+                        count++;
                     } else if (dp[i + 1][j - 1]) {
-                        ans++;
                         dp[i][j] = true;
+                        count++;
                     }
                 }
             }
         }
-        return ans;
+        return count;
     }
 }
