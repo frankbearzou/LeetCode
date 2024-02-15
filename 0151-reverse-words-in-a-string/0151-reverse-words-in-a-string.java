39acad2a098ae96964c846b9d4c986fd2825d5c3
Frank Zou <frankbearzou@gmail.com>
2024-02-14 21:37:40 -0500

    Time: 5 ms (92.17%), Space: 43 MB (73.28%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
@@ -1,50 +1,12 @@
 class Solution {
     public String reverseWords(String s) {
-        int l = 0, r = s.length() - 1;
-        // l start
-        for (int i = 0; i < s.length(); i++) {
-            if (s.charAt(l) == ' ')
-                l++;
-            else
-                break;
-        }
-        // r end
-        for (int i = s.length() - 1; i >= 0; i--) {
-            if (s.charAt(i) == ' ')
-                r--;
-            else
-                break;
-        }
         StringBuilder sb = new StringBuilder();
-        for (int i = l; i <= r; i++) {
-            char c = s.charAt(i);
-            if (c != ' ' || sb.charAt(sb.length() - 1) != ' ') {
-                sb.append(c);
-            }
+        String[] split = s.trim().split(" ");
+        for (int i = split.length - 1; i >= 0; i--) {
+            if (split[i].length() == 0)
+                continue;
+            sb.append(split[i]).append(" ");
         }
-        l = 0; r = sb.length() - 1;
-        // reverse whole string
-        while (l < r) {
-            char c = sb.charAt(l);
-            sb.setCharAt(l, sb.charAt(r));
-            sb.setCharAt(r, c);
-            l++; r--;
-        }
-        // reverse word
-        int start = 0, end = start + 1;
-        while (end < sb.length()) {
-            while (end < sb.length() && sb.charAt(end) != ' ')
-                end++;
-            l = start; r = end - 1;
-            while (l < r) {
-                char c = sb.charAt(l);
-                sb.setCharAt(l, sb.charAt(r));
-                sb.setCharAt(r, c);
-                l++; r--;
-            }
-            start = end + 1;
-            end = start + 1;
-        }
-        return sb.toString();
+        return sb.toString().trim();
     }
 }
