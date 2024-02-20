e4f0d751fe3215618934da9787cff3983fa8206e
Frank Zou <frankbearzou@gmail.com>
2024-02-20 16:21:54 -0500

    Time: 6 ms (54.06%), Space: 46.1 MB (10.98%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.java
+++ 0207-course-schedule/0207-course-schedule.java
@@ -1,13 +1,13 @@
 class Solution {
     public boolean canFinish(int numCourses, int[][] prerequisites) {
         Map<Integer, List<Integer>> map = new HashMap<>();
+        Set<Integer> visited = new HashSet<>();
         for (int[] pre : prerequisites) {
             List<Integer> list = map.getOrDefault(pre[0], new ArrayList());
             list.add(pre[1]);
             map.put(pre[0], list);
         }
         for (int i = 0; i < numCourses; i++) {
-            Set<Integer> visited = new HashSet<>();
             if (!dfs(map, i, visited))
                 return false;
         }
@@ -17,13 +17,15 @@ class Solution {
     boolean dfs(Map<Integer, List<Integer>> map, int i, Set<Integer> visited) {
         if (visited.contains(i))
             return false;
-        visited.add(i);
         if (!map.containsKey(i))
             return true;
+        visited.add(i);
         for (int nei : map.get(i)) {
             if (!dfs(map, nei, visited))
                 return false;
         }
+        visited.remove(i);
+        map.remove(i);
         return true;
     }
 }
