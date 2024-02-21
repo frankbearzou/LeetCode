5040c20d23962ae9b54600bc1936ed7d5bb3b1b8
Frank Zou <frankbearzou@gmail.com>
2024-02-21 09:57:17 -0500

    Time: 210 ms (29.46%), Space: 41.1 MB (97.17%) - LeetHub

--- 0079-word-search/0079-word-search.java
+++ 0079-word-search/0079-word-search.java
@@ -1,44 +1,42 @@
 class Solution {
-    private boolean ans = false;
-    
+    int[][] dirs = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+    int row;
+    int col;
+
     public boolean exist(char[][] board, String word) {
-        for (int i = 0; i < board.length; i++) {
-            for (int j = 0; j < board[0].length; j++) {
-                if (word.charAt(0) == board[i][j]) {
-                    dfs(board, i, j, word, 0, new boolean[board.length][board[0].length]);
-                    if (ans)
+        row = board.length;
+        col = board[0].length;
+        boolean[][] visited = new boolean[row][col];
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                if (board[i][j] == word.charAt(0)) {
+                    if (dfs(board, word, 0, i, j, visited))
                         return true;
                 }
             }
         }
-        
         return false;
     }
-    
-    private void dfs(char[][] board, int row, int col, String word, int pos, boolean[][] visited) {
-        if (ans)
-            return;
-        
-        if (row < 0 || row >= board.length || col < 0 || col >= board[0].length || 
-            pos >= word.length() || word.charAt(pos) != board[row][col] || visited[row][col]) {
-            return;
-        }
-        
-        if (pos == word.length() - 1) {
-            ans = true;
-            return;
-        }
-        
-        visited[row][col] = true;
-        if (row > 0)
-            dfs(board, row - 1, col, word, pos + 1, visited);
-        if (row < board.length - 1)
-            dfs(board, row + 1, col, word, pos + 1, visited);
-        if (col > 0)
-            dfs(board, row, col - 1, word, pos + 1, visited);
-        if (col < board[0].length - 1)
-            dfs(board, row, col + 1, word, pos + 1, visited);
 
-        visited[row][col] = false;
+    boolean dfs(char[][] board, String word, int index, int r, int c, boolean[][] visited) {
+        if (index >= word.length())
+            return false;
+        if (visited[r][c])
+            return false;
+        if (index == word.length() - 1 && board[r][c] == word.charAt(index))
+            return true;
+        if (board[r][c] != word.charAt(index))
+            return false;
+            visited[r][c] = true;
+        for (int[] dir : dirs) {
+            int newr = r + dir[0];
+            int newc = c + dir[1];
+            if (newr < 0 || newr >= row || newc < 0 || newc >= col)
+                continue;
+            if (dfs(board, word, index + 1, newr, newc, visited))
+                return true;
+        }
+        visited[r][c] = false;
+        return false;
     }
 }
