bce83597cd662acb83d8c6ca5a875d0e7765133e
Frank Zou <frankbearzou@gmail.com>
2024-02-23 09:16:23 -0500

    Time: 27 ms (7.23%), Space: 58.1 MB (96.52%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -1,14 +1,13 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        int ans = 0;
-        int low = prices[0];
-        for (int i = 1; i < prices.length; i++) {
-            if (prices[i] < low) {
-                low = prices[i];
-            } else {
-                ans = Math.max(ans, prices[i] - low);
-            }
+        int n = prices.length;
+        int[][] dp = new int[n][2];
+        dp[0][0] = -prices[0]; // funds with stock
+        dp[0][1] = 0; // funds without stock: not buy yet or sold
+        for (int i = 1; i < n; i++) {
+            dp[i][0] = Math.max(dp[i - 1][0], -prices[i]);
+            dp[i][1] = Math.max(dp[i - 1][1], dp[i - 1][0] + prices[i]);
         }
-        return ans;
+        return dp[n - 1][1];
     }
 }
