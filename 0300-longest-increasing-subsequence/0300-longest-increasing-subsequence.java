32790bc5beb23cbdfacee0c612afb7577c191f0c
Frank Zou <frankbearzou@gmail.com>
2024-02-24 11:25:35 -0500

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -1,9 +1,10 @@
 class Solution {
     public int lengthOfLIS(int[] nums) {
         int ans = 0;
-        int[] dp = new int[nums.length];
+        int n = nums.length;
+        int[] dp = new int[n];
         Arrays.fill(dp, 1);
-        for (int i = 0; i < dp.length; i++) {
+        for (int i = 1; i < n; i++) {
             for (int j = 0; j < i; j++) {
                 if (nums[j] < nums[i]) {
                     dp[i] = Math.max(dp[i], dp[j] + 1);
