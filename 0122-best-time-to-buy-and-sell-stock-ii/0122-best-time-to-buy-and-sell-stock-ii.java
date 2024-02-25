b463ec20a644aecdd576a1b7b03dae3e6f0846f7
Frank Zou <frankbearzou@gmail.com>
2024-02-24 21:46:14 -0500

    Time: 1 ms (93.79%), Space: 46 MB (27.15%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
@@ -1,12 +1,11 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        int[][] dp = new int[prices.length][2];
-        dp[0][0] = -prices[0]; // hold stock
-        dp[0][1] = 0; // without stock
+        int ans = 0;
         for (int i = 1; i < prices.length; i++) {
-            dp[i][0] = Math.max(dp[i - 1][0], dp[i - 1][1] - prices[i]);
-            dp[i][1] = Math.max(dp[i - 1][1], dp[i - 1][0] + prices[i]);
+            if (prices[i] > prices[i - 1]) {
+                ans += prices[i] - prices[i - 1];
+            }
         }
-        return dp[dp.length - 1][1];
+        return ans;
     }
 }
