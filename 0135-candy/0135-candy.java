e25e68bf4a5f0c97020df45f4b0e4b8e7e9a7d24
Frank Zou <frankbearzou@gmail.com>
2024-02-25 21:07:51 -0500

    Time: 4 ms (43.38%), Space: 45.5 MB (48.49%) - LeetHub

--- 0135-candy/0135-candy.java
+++ 0135-candy/0135-candy.java
@@ -1,19 +1,20 @@
 class Solution {
     public int candy(int[] ratings) {
-        int sum = 0;
-        int[] candy = new int[ratings.length];
-        for (int i = 0; i < candy.length; i++)
-            candy[i] = 1;
-        for (int i = 1; i < candy.length; i++) {
-            if (ratings[i - 1] < ratings[i] && candy[i - 1] >= candy[i])
-                candy[i] = candy[i - 1] + 1;
+        int n = ratings.length;
+        int[] arr = new int[n];
+        Arrays.fill(arr, 1);
+        int count = 0;
+        for (int i = 1; i < n; i++) {
+            if (ratings[i] > ratings[i - 1] && arr[i] <= arr[i - 1])
+                arr[i] = arr[i - 1] + 1;
         }
-        for (int i = candy.length - 2; i >= 0; i--) {
-            if (ratings[i] > ratings[i + 1] && candy[i] <= candy[i + 1])
-                candy[i] = candy[i + 1] + 1;
+        for (int i = n - 2; i >= 0; i--) {
+            if (ratings[i] > ratings[i + 1] && arr[i] <= arr[i + 1])
+                arr[i] = arr[i + 1] + 1;
         }
-        for (int i = 0; i < candy.length; i++)
-            sum += candy[i];
-        return sum;
+        for (int c : arr) {
+            count += c;
+        }
+        return count;
     }
 }
