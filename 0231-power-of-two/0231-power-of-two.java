22ce701edcf3614f05e2247985480361345a4ded
Frank Zou <frankbearzou@gmail.com>
2024-02-26 10:23:44 -0500

    Time: 0 ms (100%), Space: 40.2 MB (98.94%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.java
+++ 0231-power-of-two/0231-power-of-two.java
@@ -1,12 +1,5 @@
 class Solution {
     public boolean isPowerOfTwo(int n) {
-        int count = 0;
-        while (n > 0) {
-            if ((n & 1) == 1) {
-                count++;
-            }
-            n >>= 1;
-        }
-        return count == 1;
+        return n > 0 && (n & (n - 1)) == 0;
     }
 }
