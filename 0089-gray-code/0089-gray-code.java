4969c08dd3bf1f68221bc7b1cb7bd1dbc7ee282c
Frank Zou <frankbearzou@gmail.com>
2024-02-27 22:15:55 -0500

    Time: 7 ms (70.46%), Space: 51.8 MB (39.95%) - LeetHub

---
+++ 0089-gray-code/0089-gray-code.java
@@ -0,0 +1,14 @@
+class Solution {
+    public List<Integer> grayCode(int n) {
+        List<Integer> ans = new ArrayList<>();
+        ans.add(0);
+        int head = 1;
+        for (int i = 0; i < n; i++) {
+            for (int j = ans.size() - 1; j >= 0; j--) {
+                ans.add(head + ans.get(j));
+            }
+            head <<= 1;
+        }
+        return ans;
+    }
+}
