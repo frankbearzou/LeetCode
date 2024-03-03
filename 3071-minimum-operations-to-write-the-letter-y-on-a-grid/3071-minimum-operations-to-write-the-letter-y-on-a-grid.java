bf1ec6cee93989e05dc0c0dabec22edc531fb113
Frank Zou <frankbearzou@gmail.com>
2024-03-03 14:42:25 -0500

    Time: 2 ms (50%), Space: 44.7 MB (100%) - LeetHub

--- 3071-minimum-operations-to-write-the-letter-y-on-a-grid/3071-minimum-operations-to-write-the-letter-y-on-a-grid.java
+++ 3071-minimum-operations-to-write-the-letter-y-on-a-grid/3071-minimum-operations-to-write-the-letter-y-on-a-grid.java
@@ -12,12 +12,6 @@ class Solution {
                 }
             }
         }
-        for (int i = 0; i < 3; i++) {
-            System.out.println(a[i]);
-        }
-        for (int i = 0; i < 3; i++) {
-            System.out.println(b[i]);
-        }
         int noChange = 0;
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
