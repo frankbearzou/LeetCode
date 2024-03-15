08029ecd977248be2e57902dad6343599147bb56
Frank Zou <frankbearzou@gmail.com>
2024-03-14 23:15:30 -0400

    Time: 0 ms (100%), Space: 42.3 MB (21.14%) - LeetHub

--- 1122-relative-sort-array/1122-relative-sort-array.java
+++ 1122-relative-sort-array/1122-relative-sort-array.java
@@ -1,28 +1,24 @@
 class Solution {
     public int[] relativeSortArray(int[] arr1, int[] arr2) {
-        int n = arr1.length;
-        int[] res = new int[n];
-        Map<Integer, Integer> map = new HashMap<>();
-        Set<Integer> set = new HashSet<>();
-        Queue<Integer> queue = new PriorityQueue<>();
-        for (int num : arr2) {
-            set.add(num);
-        }
+        int[] res = new int[arr1.length];
+        int[] map = new int[1001];
         for (int num : arr1) {
-            if (set.contains(num))
-                map.put(num, map.getOrDefault(num, 0) + 1);
-            else
-                queue.offer(num);
+            map[num]++;
         }
         int index = 0;
         for (int num : arr2) {
-            int count = map.get(num);
-            while (count-- > 0) {
-                res[index++] = num;
+            if (map[num] > 0) {
+                while (map[num]-- > 0) {
+                    res[index++] = num;
+                }
             }
         }
-        while (!queue.isEmpty()) {
-            res[index++] = queue.poll();
+        for (int i = 0; i < 1001; i++) {
+            if (map[i] > 0) {
+                while (map[i]-- > 0) {
+                    res[index++] = i;
+                }
+            }
         }
         return res;
     }
