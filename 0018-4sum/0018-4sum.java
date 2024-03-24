fd89053d65d83e275e595a0fbfdbac088a422f55
Frank Zou <frankbearzou@gmail.com>
2024-03-24 12:31:06 -0400

    Time: 4 ms (95.6%), Space: 43.2 MB (98.59%) - LeetHub

--- 0018-4sum/0018-4sum.java
+++ 0018-4sum/0018-4sum.java
@@ -1,36 +1,39 @@
 class Solution {
     public List<List<Integer>> fourSum(int[] nums, int target) {
         List<List<Integer>> res = new ArrayList<>();
-        Arrays.sort(nums);
         int n = nums.length;
+        long[] num = new long[n];
+        for (int i = 0; i < n; i++)
+            num[i] = nums[i];
+        Arrays.sort(num);
         for (int a = 0; a < n - 3; a++) {
-            if (a > 0 && nums[a - 1] == nums[a])
+            if (a > 0 && num[a - 1] == num[a])
                 continue;
             for (int b = a + 1; b < n - 2; b++) {
-                if (b - a > 1 && nums[b - 1] == nums[b])
+                if (b - a > 1 && num[b - 1] == num[b])
                     continue;
-                if (nums[a] + nums[b] + nums[b + 1] + nums[b + 2] > target)
+                if (num[a] + num[b] + num[b + 1] + num[b + 2] > target)
                     break;
-                if (nums[a] + nums[b] + nums[n - 2] + nums[n - 1] < target)
+                if (num[a] + num[b] + num[n - 2] + num[n - 1] < target)
                     continue;
                 int c = b + 1, d = n - 1;
                 while (c < d) {
-                    int sum = nums[a] + nums[b] + nums[c] + nums[d];
+                    long sum = num[a] + num[b] + num[c] + num[d];
                     if (sum < target) {
                         c++;
-                        while (c < d && nums[c - 1] == nums[c])
+                        while (c < d && num[c - 1] == num[c])
                             c++;
                     } else if (sum > target) {
                         d--;
-                        while (c < d && nums[d] == nums[d + 1])
+                        while (c < d && num[d] == num[d + 1])
                             d--;
                     } else {
-                        res.add(List.of(nums[a], nums[b], nums[c], nums[d]));
+                        res.add(List.of((int)num[a], (int)num[b], (int)num[c], (int)num[d]));
                         c++;
-                        while (c < d && nums[c - 1] == nums[c])
+                        while (c < d && num[c - 1] == num[c])
                             c++;
                         d--;
-                        while (c < d && nums[d] == nums[d + 1])
+                        while (c < d && num[d] == num[d + 1])
                             d--;
                     }
                 }
