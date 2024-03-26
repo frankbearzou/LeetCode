8d70906ecb1144f026daab2c25d702ba767ba6a3
Frank Zou <frankbearzou@gmail.com>
2024-03-26 09:15:55 -0400

    Time: 6 ms (43.31%), Space: 44.8 MB (92.99%) - LeetHub

--- 2730-find-the-longest-semi-repetitive-substring/2730-find-the-longest-semi-repetitive-substring.java
+++ 2730-find-the-longest-semi-repetitive-substring/2730-find-the-longest-semi-repetitive-substring.java
@@ -1,18 +1,13 @@
 class Solution {
     public int longestSemiRepetitiveSubstring(String s) {
         int res = 1;
-        int l = 0;
-        int same = 0;
-        char[] arr = s.toCharArray();
-        int n = arr.length;
-        for (int r = 1; r < n; r++) {
-            if (arr[r - 1] == arr[r] && ++same > 1) {
-                l++;
-                while (arr[l - 1] != arr[l])
-                    l++;
-                same = 1;
+        int pre = 0, cur = 0;
+        for (int i = 1; i < s.length(); i++) {
+            if (s.charAt(i - 1) == s.charAt(i)) {
+                pre = cur;
+                cur = i;
             }
-            res = Math.max(res, r - l + 1);
+            res = Math.max(res, i - pre + 1);
         }
         return res;
     }
