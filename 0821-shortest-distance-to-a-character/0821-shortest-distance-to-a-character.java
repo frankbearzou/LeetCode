33cab65959aeeb0284392b4b223606f49ca197eb
Frank Zou <frankbearzou@gmail.com>
2024-03-31 18:33:20 -0400

    Time: 1 ms (99.25%), Space: 42.5 MB (56.02%) - LeetHub

--- 0821-shortest-distance-to-a-character/0821-shortest-distance-to-a-character.java
+++ 0821-shortest-distance-to-a-character/0821-shortest-distance-to-a-character.java
@@ -1,28 +1,18 @@
 class Solution {
     public int[] shortestToChar(String s, char c) {
-        int diff = -1;
         char[] arr = s.toCharArray();
         int n = arr.length;
         int[] ans = new int[n];
-        Arrays.fill(ans, 10000);
-        // left to right
+        int pos = -n;
         for (int i = 0; i < n; i++) {
-            if (arr[i] == c) {
-                ans[i] = 0;
-                diff = 1;
-            } else if (diff != -1) {
-                ans[i] = Math.min(ans[i], diff++);
-            }
+            if (arr[i] == c)
+                pos = i;
+            ans[i] = i - pos;
         }
-        diff = -1;
-        // right to left
-        for (int i = n - 1; i >= 0; i--) {
-            if (arr[i] == c) {
-                ans[i] = 0;
-                diff = 1;
-            } else if (diff != -1) {
-                ans[i] = Math.min(ans[i], diff++);
-            }
+        for (int i = pos - 1; i >= 0; i--) {
+            if (arr[i] == c)
+                pos = i;
+            ans[i] = Math.min(ans[i], pos - i);
         }
         return ans;
     }
