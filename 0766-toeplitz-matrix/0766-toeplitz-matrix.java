d89b412c9c1d947967c555d363ae8fdffa6fb966
Frank Zou <frankbearzou@gmail.com>
2024-03-30 22:55:40 -0400

    Time: 0 ms (100%), Space: 44.4 MB (35.02%) - LeetHub

--- 0766-toeplitz-matrix/0766-toeplitz-matrix.java
+++ 0766-toeplitz-matrix/0766-toeplitz-matrix.java
@@ -1,19 +1,8 @@
 class Solution {
     public boolean isToeplitzMatrix(int[][] matrix) {
-        int row = matrix.length, col = matrix[0].length;
-        // bottom to up
-        for (int r = row - 1; r >= 0; r--) {
-            int val = matrix[r][0];
-            for (int i = r + 1, j = 1; i < row && j < col; i++, j++) {
-                if (matrix[i][j] != val)
-                    return false;
-            }
-        }
-        // left to right
-        for (int c = 1; c < col; c++) {
-            int val = matrix[0][c];
-            for (int i = 1, j = c + 1; i < row && j < col; i++, j++) {
-                if (matrix[i][j] != val)
+        for (int i = 0; i < matrix.length - 1; i++) {
+            for (int j = 0; j < matrix[0].length - 1; j++) {
+                if (matrix[i][j] != matrix[i + 1][j + 1])
                     return false;
             }
         }
