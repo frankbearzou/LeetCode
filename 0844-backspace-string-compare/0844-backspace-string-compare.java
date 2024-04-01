782b55b4efee93709cc0c003064baa0a4dc0ee1a
Frank Zou <frankbearzou@gmail.com>
2024-03-31 22:00:22 -0400

    Time: 0 ms (100%), Space: 41 MB (96.81%) - LeetHub

--- 0844-backspace-string-compare/0844-backspace-string-compare.java
+++ 0844-backspace-string-compare/0844-backspace-string-compare.java
@@ -1,21 +1,34 @@
 class Solution {
     public boolean backspaceCompare(String s, String t) {
-        Stack<Character> a = new Stack<>();
-        Stack<Character> b = new Stack<>();
-        helper(s, a);
-        helper(t, b);
-        return a.equals(b);
-    }
-
-    void helper(String str, Stack<Character> stack) {
-        for (char c : str.toCharArray()) {
-            if (c == '#') {
-                if (!stack.isEmpty()) {
-                    stack.pop();
-                }
-            } else {
-                stack.push(c);
+        int i = s.length(), j = t.length();
+        while (i >= 0 && j >= 0) {
+            int back = 0;
+            i--;
+            j--;
+            while (i >= 0) {
+                if (s.charAt(i) == '#') {
+                    back++;
+                    i--;
+                } else if (back > 0) {
+                    back--;
+                    i--;
+                } else
+                    break;
             }
+            back = 0;
+            while (j >= 0) {
+                if (t.charAt(j) == '#') {
+                    back++;
+                    j--;
+                } else if (back > 0) {
+                    back--;
+                    j--;
+                } else
+                    break;
+            }
+            if (i >= 0 && j >= 0 && s.charAt(i) != t.charAt(j))
+                break;
         }
+        return i == -1 && j == -1;
     }
 }
