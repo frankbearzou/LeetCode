8cd82cf61751959c8c95406a1228adfa3f80b7fe
Frank Zou <frankbearzou@gmail.com>
2024-04-02 08:32:06 -0400

    Time: 0 ms (100%), Space: 46.1 MB (16.34%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,40 +1,21 @@
 class Solution {
     public int[] searchRange(int[] nums, int target) {
-        if (nums.length == 0 || target < nums[0] || target > nums[nums.length - 1])
+        int l = lowerBound(nums, target);
+        if (l == nums.length || nums[l] != target)
             return new int[]{-1, -1};
-        // left
-        int left = -1, right = -1;
-        int l = 0, r = nums.length - 1;
+        int r = lowerBound(nums, target + 1) - 1;
+        return new int[]{l, r};
+    }
+
+    int lowerBound(int[] nums, int target) {
+        int l = 0 , r = nums.length - 1;
         while (l <= r) {
-            int mid = l + (r - l) / 2;
-            if (nums[mid] >= target) {
-                r = mid - 1;
-            } else {
-                l = mid + 1;
-            }
+            int m = l + (r - l) / 2;
+            if (nums[m] < target)
+                l = m + 1;
+            else
+                r = m - 1;
         }
-        if (r >= 0 && r < nums.length && nums[r] == target)
-            left = r;
-        else if (l >= 0 && l < nums.length && nums[l] == target)
-            left = l;
-        else
-            return new int[]{-1, -1};
-        // right
-        l = 0; r = nums.length - 1;
-        while (l <= r) {
-            int mid = l + (r - l) / 2;
-            if (nums[mid] <= target) {
-                l = mid + 1;
-            } else {
-                r = mid - 1;
-            }
-        }
-        if (l >= 0 && l < nums.length && nums[l] == target)
-            right = l;
-        else if (r >= 0 && r < nums.length && nums[r] == target)
-            right = r;
-        else
-            return new int[]{-1, -1};
-        return new int[]{left, right};
+        return l;
     }
 }
