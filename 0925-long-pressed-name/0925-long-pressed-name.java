ad3ead455227e41ef75ca2ec8153a960ed3aa65a
Frank Zou <frankbearzou@gmail.com>
2024-04-02 20:06:33 -0400

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0925-long-pressed-name/0925-long-pressed-name.java
+++ 0925-long-pressed-name/0925-long-pressed-name.java
@@ -1,30 +1,19 @@
 class Solution {
     public boolean isLongPressedName(String name, String typed) {
         int i = 0, j = 0;
-        char[] arr = name.toCharArray();
-        char[] t = typed.toCharArray();
-        if (arr[0] != t[0])
-            return false;
-        if (arr[arr.length - 1] != t[t.length - 1])
-            return false;
-        while (i < arr.length && j < t.length) {
-            if (arr[i] == t[j]) {
+        while (i < name.length() && j < typed.length()) {
+            if (name.charAt(i) == typed.charAt(j)) {
                 i++;
                 j++;
-            } else {
-                if (t[j - 1] != t[j])
-                    return false;
-                else
-                    j++;
-            }
-        }
-        if (i < arr.length)
-            return false;
-        while (j < t.length) {
-            if (t[j] != t[j - 1])
+            } else if (j > 0 && (typed.charAt(j - 1) == typed.charAt(j))) {
+                j++;
+            } else
                 return false;
-            j++;
         }
+        if (i < name.length())
+            return false;
+        while (j < typed.length() && typed.charAt(j - 1) != typed.charAt(j))
+            return false;
         return true;
     }
 }
