4e08e5e4773ef1b5d1060c113d1fa1081dd12811
Frank Zou <frankbearzou@gmail.com>
2024-04-03 23:37:07 -0400

    Time: 3 ms (79.6%), Space: 42.6 MB (20.26%) - LeetHub

--- 1005-maximize-sum-of-array-after-k-negations/1005-maximize-sum-of-array-after-k-negations.java
+++ 1005-maximize-sum-of-array-after-k-negations/1005-maximize-sum-of-array-after-k-negations.java
@@ -3,20 +3,19 @@ class Solution {
         Arrays.sort(nums);
         for (int i = 0; i < nums.length; i++) {
             if (nums[i] < 0 && k > 0) {
-                nums[i] *= - 1;
+                nums[i] *= -1;
                 k--;
             }
         }
-        if (k > 0)
-            Arrays.sort(nums);
-        while (k > 0) {
-            nums[0] *= -1;
-            k--;
-        }
+        int min = 10001;
         int sum = 0;
         for (int num : nums) {
             sum += num;
+            min = Math.min(min, num);
         }
-        return sum;
+        if (k % 2 == 0)
+            return sum;
+        else
+            return sum - 2 * min;
     }
 }
