09595e9aedabbb8e16c2dd1f67bf1972739765f8
Frank Zou <frankbearzou@gmail.com>
2024-04-12 23:58:04 -0400

    Time: 17 ms (48.16%), Space: 46.8 MB (25.31%) - LeetHub

---
+++ 0085-maximal-rectangle/0085-maximal-rectangle.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int maximalRectangle(char[][] matrix) {
+        int res = 0;
+        int row = matrix.length, col = matrix[0].length;
+        int[][] dp = new int[row][col];
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                if (matrix[i][j] == '1') {
+                    dp[i][j] = j == 0 ? 1 : dp[i][j - 1] + 1;
+                    int width = dp[i][j];
+                    for (int k = i; k >= 0; k--) {
+                        width = Math.min(width, dp[k][j]);
+                        res = Math.max(res, width * (i - k + 1));
+                    }
+                }
+            }
+        }
+        return res;
+    }
+}
