ea22d987d1d8470dafbbfc30a6740ab3f6dbbf5f
Frank Zou <frankbearzou@gmail.com>
2024-04-18 23:27:30 -0400

    Time: 3 ms (86.63%), Space: 49.3 MB (66.81%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -1,35 +1,27 @@
 class Solution {
     public int numIslands(char[][] grid) {
-        int ans = 0;
-        boolean[][] visited = new boolean[grid.length][grid[0].length];
+        int count = 0;
         for (int i = 0; i < grid.length; i++) {
             for (int j = 0; j < grid[0].length; j++) {
-                if (!visited[i][j] && grid[i][j] == '1') {
-                    ans++;
-                    bfs(grid, i, j, visited);
+                if (grid[i][j] == '1') {
+                    count++;
+                    dfs(grid, i, j);
                 }
             }
         }
-        return ans;
+        return count;
     }
 
-    void bfs(char[][] grid, int row, int col, boolean[][] visited) {
-        Queue<int[]> queue = new LinkedList<>();
-        queue.offer(new int[]{row, col});
-        visited[row][col] = true;
-        while (!queue.isEmpty()) {
-            int[] poll = queue.poll();
-            int[][] dirs = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
-            for (int[] dir : dirs) {
-                int newx = poll[0] + dir[0];
-                int newy = poll[1] + dir[1];
-                if (newx < 0 || newx >= grid.length || newy < 0 || newy >= grid[0].length)
-                    continue;
-                if (!visited[newx][newy] && grid[newx][newy] == '1') {
-                    queue.offer(new int[]{newx, newy});
-                    visited[newx][newy] = true;
-                }
-            }
+    int[][] dirs = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+
+    void dfs(char[][] grid, int r, int c) {
+        grid[r][c] = '0';
+        for (int[] dir : dirs) {
+            int newr = r + dir[0];
+            int newc = c + dir[1];
+            if (newr < 0 || newr >= grid.length || newc < 0 || newc >= grid[0].length || grid[newr][newc] == '0')
+                continue;
+            dfs(grid, newr, newc);
         }
     }
 }
