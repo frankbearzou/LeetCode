e4c81548b5951940e5abaa07003164c307fd66b7
Frank Zou <frankbearzou@gmail.com>
2024-05-02 14:01:05 -0400

    Time: 102 ms (17.31%), Space: 45.8 MB (7.08%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.java
+++ 0076-minimum-window-substring/0076-minimum-window-substring.java
@@ -2,38 +2,26 @@ class Solution {
     public String minWindow(String s, String t) {
         int len = Integer.MAX_VALUE;
         String res = "";
-        Map<Character, Integer> m1 = new HashMap<>();
-        Map<Character, Integer> m2 = new HashMap<>();
-        for (char c : t.toCharArray()) {
-            m2.put(c, m2.getOrDefault(c, 0) + 1);
-        }
+        int[] map = new int[128];
+        for (char c : t.toCharArray())
+            map[c]++;
+        int count = t.length();
         int l = 0, n = s.length();
+        char[] arr = s.toCharArray();
         for (int r = 0; r < n; r++) {
-            char c = s.charAt(r);
-            if (m2.containsKey(c)) {
-                m1.put(c, m1.getOrDefault(c, 0) + 1);
-                while (contains(m1, m2)) {
-                    if (r - l + 1 < len) {
-                        len = r - l + 1;
-                        res = s.substring(l, r + 1);
-                    }
-                    if (m1.containsKey(s.charAt(l))) {
-                        m1.put(s.charAt(l), m1.get(s.charAt(l)) - 1);
-                    }
-                    l++;
+            if (map[arr[r]]-- > 0) {
+                count--;
+            }
+            while (count == 0) {
+                if (r - l + 1 < len) {
+                    len = r - l + 1;
+                    res = s.substring(l, r + 1);
                 }
+                if (map[arr[l]] == 0)
+                    count++;
+                map[arr[l++]]++;
             }
         }
         return res;
-    }
-
-    boolean contains(Map<Character, Integer> m1, Map<Character, Integer> m2) {
-        if (m1.size() != m2.size())
-            return false;
-        for (var entry : m2.entrySet()) {
-            if (m1.getOrDefault(entry.getKey(), 0) < m2.get(entry.getKey()))
-                return false;
-        }
-        return true;
     }
 }
