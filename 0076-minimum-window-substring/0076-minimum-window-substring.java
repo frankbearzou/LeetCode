4ebfb0b7d21f1157ab4b0c39dde24f06f8518ad6
Frank Zou <frankbearzou@gmail.com>
2024-05-02 11:58:40 -0400

    Time: 386 ms (5.02%), Space: 44.9 MB (53.4%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.java
+++ 0076-minimum-window-substring/0076-minimum-window-substring.java
@@ -1,34 +1,39 @@
 class Solution {
     public String minWindow(String s, String t) {
-        if (t.length() > s.length())
-            return "";
-        String ans = "";
-        int minLen = Integer.MAX_VALUE;
-        int[] map = new int[128];
+        int len = Integer.MAX_VALUE;
+        String res = "";
+        Map<Character, Integer> m1 = new HashMap<>();
+        Map<Character, Integer> m2 = new HashMap<>();
         for (char c : t.toCharArray()) {
-            map[c]++;
+            m2.put(c, m2.getOrDefault(c, 0) + 1);
         }
-        int count = t.length();
-        int l = 0, r = 0;
-        char[] arr = s.toCharArray();
-        while (r < arr.length) {
-            if (map[arr[r]] > 0) {
-                count--;
-            }
-            map[arr[r]]--;
-            r++;
-            while (count == 0) {
-                if (r - l < minLen) {
-                    minLen = r - l;
-                    ans = s.substring(l, r);
+        int l = 0, n = s.length();
+        for (int r = 0; r < n; r++) {
+            char c = s.charAt(r);
+            if (m2.containsKey(c)) {
+                m1.put(c, m1.getOrDefault(c, 0) + 1);
+                while (contains(m1, m2)) {
+                    if (r - l + 1 < len) {
+                        len = r - l + 1;
+                        res = s.substring(l, r + 1);
+                    }
+                    if (m1.containsKey(s.charAt(l))) {
+                        m1.put(s.charAt(l), m1.get(s.charAt(l)) - 1);
+                    }
+                    l++;
                 }
-                if (map[arr[l]] == 0) {
-                    count++;
-                }
-                map[arr[l]]++;
-                l++;
             }
         }
-        return ans;
+        return res;
+    }
+
+    boolean contains(Map<Character, Integer> m1, Map<Character, Integer> m2) {
+        if (m1.size() != m2.size())
+            return false;
+        for (var entry : m2.entrySet()) {
+            if (m1.getOrDefault(entry.getKey(), 0) < m2.get(entry.getKey()))
+                return false;
+        }
+        return true;
     }
 }
