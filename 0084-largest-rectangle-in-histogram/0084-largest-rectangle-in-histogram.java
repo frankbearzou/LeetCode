d3c62d919369691e7fceb10325eafe159b3566f0
Frank Zou <frankbearzou@gmail.com>
2024-05-07 11:22:50 -0400

    Time: 63 ms (67.58%), Space: 60.6 MB (9.13%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.java
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.java
@@ -1,31 +1,19 @@
 class Solution {
     public int largestRectangleArea(int[] heights) {
-        int ans = 0;
-        int N = heights.length;
-        // left min
-        int[] lmin = new int[N];
-        lmin[0] = -1;
-        for (int i = 1; i < N; i++) {
-            int l = i - 1;
-            while (l >= 0 && heights[l] >= heights[i]) {
-                l = lmin[l];
+        int res = 0;
+        Stack<Integer> stack = new Stack<>();
+        int[] arr = new int[heights.length + 2];
+        for (int i = 0; i < heights.length; i++) {
+            arr[i + 1] = heights[i];
+        }
+        for (int i = 0; i < arr.length; i++) {
+            while (!stack.isEmpty() && arr[i] < arr[stack.peek()]) {
+                int h = arr[stack.pop()];
+                int w = i - stack.peek() - 1;
+                res = Math.max(res, h * w);
             }
-            lmin[i] = l;
+            stack.push(i);
         }
-        // right min
-        int[] rmin = new int[N];
-        rmin[N - 1] = N;
-        for (int i = N - 2; i >= 0; i--) {
-            int r = i + 1;
-            while (r < N && heights[r] >= heights[i]) {
-                r = rmin[r];
-            }
-            rmin[i] = r;
-        }
-        for (int i = 0; i < N; i++) {
-            int sum = heights[i] * (rmin[i] - lmin[i] - 1);
-            ans = Math.max(ans, sum);
-        }
-        return ans;
+        return res;
     }
 }
