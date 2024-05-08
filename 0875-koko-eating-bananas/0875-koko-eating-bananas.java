a4648354c74e6842099409586d1c5b51cfe48dd6
Frank Zou <frankbearzou@gmail.com>
2024-05-08 09:19:29 -0400

    Time: 28 ms (20.2%), Space: 45 MB (57%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.java
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.java
@@ -1,25 +1,19 @@
 class Solution {
     public int minEatingSpeed(int[] piles, int h) {
-        int l = 1, r = 0;
+        int l = 1, r = 1;
         for (int pile : piles)
             r = Math.max(r, pile);
         while (l < r) {
             int m = l + (r - l) / 2;
-            if (check(piles, h, m))
-                r = m;
-            else
+            int k = 0;
+            for (int pile : piles) {
+                k += Math.ceil(1.0 * pile / m);
+            }
+            if (k > h)
                 l = m + 1;
+            else
+                r = m;
         }
         return l;
-    }
-
-    boolean check(int[] piles, int h, int m) {
-        int sum = 0;
-        for (int pile : piles) {
-            sum += (pile - 1) / m + 1;
-            if (sum > h)
-                return false;
-        }
-        return true;
     }
 }
