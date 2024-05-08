290e2bf532c9e4f1f5dc99b4469f515fb55a5c26
Frank Zou <frankbearzou@gmail.com>
2024-05-07 22:17:48 -0400

    Time: 6 ms (45.15%), Space: 45.3 MB (76.34%) - LeetHub

--- 0784-letter-case-permutation/0784-letter-case-permutation.java
+++ 0784-letter-case-permutation/0784-letter-case-permutation.java
@@ -1,23 +1,22 @@
 class Solution {
-    List<String> res = new ArrayList<>();
-
     public List<String> letterCasePermutation(String s) {
-        backtracking(s, 0);
-        return res;
-    }
-
-    void backtracking(String s, int start) {
-        res.add(s);
-        for (int i = start; i < s.length(); i++) {
+        List<String> res = new ArrayList<>();
+        Queue<String> queue = new LinkedList<>();
+        queue.offer(s);
+        for (int i = 0; i < s.length(); i++) {
             char c = s.charAt(i);
             if (c >= '0' && c <= '9')
                 continue;
-            if (c >= 'A' && c <= 'Z') {
-                c = (char)(c + 32);
-            } else {
-                c = (char)(c - 32);
+            int size = queue.size();
+            while (size-- > 0) {
+                String poll = queue.poll();
+                char[] arr = poll.toCharArray();
+                arr[i] = Character.toLowerCase(arr[i]);
+                queue.offer(String.valueOf(arr));
+                arr[i] = Character.toUpperCase(arr[i]);
+                queue.offer(String.valueOf(arr));
             }
-            backtracking(s.substring(0, i) + c + s.substring(i + 1), i + 1);
         }
+        return new ArrayList(queue);
     }
 }
