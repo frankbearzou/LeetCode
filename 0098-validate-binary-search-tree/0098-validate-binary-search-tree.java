7571c2a7d100a1a93713c800160be7ecb6a309eb
Frank Zou <frankbearzou@gmail.com>
2024-05-23 09:19:12 -0400

    Time: 0 ms (100%), Space: 44.1 MB (33.85%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.java
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.java
@@ -14,15 +14,27 @@
  * }
  */
 class Solution {
+    Integer prev = null;
+    boolean valid = true;
+
     public boolean isValidBST(TreeNode root) {
-        return isValidBST(root.left, Long.MIN_VALUE, root.val) && isValidBST(root.right, root.val, Long.MAX_VALUE);
+        inorder(root);
+        return valid;
     }
 
-    boolean isValidBST(TreeNode root, long min, long max) {
+    void inorder(TreeNode root) {
         if (root == null)
-            return true;
-        if (root.val <= min || root.val >= max)
-            return false;
-        return isValidBST(root.left, min, root.val) && isValidBST(root.right, root.val, max);
+            return;
+        inorder(root.left);
+        if (prev == null) {
+            prev = root.val;
+        } else {
+            if (root.val <= prev) {
+                valid = false;
+                return;
+            }
+            prev = root.val;
+        }
+        inorder(root.right);
     }
 }
