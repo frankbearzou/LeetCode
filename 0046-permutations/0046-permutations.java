1ebe8d5459b9810c5c2068ba67df4683eb3075e7
Frank Zou <frankbearzou@gmail.com>
2024-05-29 09:51:01 -0400

    Time: 2 ms (44.92%), Space: 44.6 MB (16.36%) - LeetHub

--- 0046-permutations/0046-permutations.java
+++ 0046-permutations/0046-permutations.java
@@ -1,27 +1,22 @@
 class Solution {
-    List<List<Integer>> ans = new ArrayList<>();
-    List<Integer> path = new ArrayList<>();
-    boolean[] used;
-
     public List<List<Integer>> permute(int[] nums) {
-        used = new boolean[nums.length];
-        backtracking(nums, 0);
-        return ans;
+        List<List<Integer>> res = new ArrayList<>();
+        List<Integer> path = new ArrayList<>();
+        backtracking(nums, path, res);
+        return res;
     }
 
-    void backtracking(int[] nums, int start) {
+    void backtracking(int[] nums, List<Integer> path, List<List<Integer>> res) {
         if (path.size() == nums.length) {
-            ans.add(new ArrayList(path));
+            res.add(new ArrayList<>(path));
             return;
         }
         for (int i = 0; i < nums.length; i++) {
-            if (used[i])
+            if (path.contains(nums[i]))
                 continue;
-            used[i] = true;
             path.add(nums[i]);
-            backtracking(nums, i + 1);
+            backtracking(nums, path, res);
             path.remove(path.size() - 1);
-            used[i] = false;
         }
     }
 }
