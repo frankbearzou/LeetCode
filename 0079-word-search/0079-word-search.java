deeb3b2540024a1e97e697850df88f3a3a171ff3
Frank Zou <frankbearzou@gmail.com>
2024-06-06 08:56:33 -0400

    Time: 211 ms (21.97%), Space: 41.4 MB (69.72%) - LeetHub

--- 0079-word-search/0079-word-search.java
+++ 0079-word-search/0079-word-search.java
@@ -1,32 +1,33 @@
 class Solution {
+    int[][] dirs = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+
     public boolean exist(char[][] board, String word) {
-        int row = board.length, col = board[0].length;
-        for (int i = 0; i < row; i++) {
-            for (int j = 0; j < col; j++) {
-                boolean[][] visited = new boolean[row][col];
-                if (dfs(board, i, j, word, 0, visited))
+        boolean[][] visited = new boolean[board.length][board[0].length];
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[0].length; j++) {
+                if (dfs(board, word, i, j, 0, visited))
                     return true;
             }
         }
         return false;
     }
 
-    boolean dfs(char[][] board, int i, int j, String word, int index, boolean[][] visited) {
+    boolean dfs(char[][] board, String word, int row, int col, int index, boolean[][] visited) {
         if (index == word.length())
             return true;
-        int row = board.length, col = board[0].length;
-        if (i < 0 || i >= row || j < 0 || j >= col)
+        if (row < 0 || row >= board.length || col < 0 || col >= board[0].length)
             return false;
-        if (visited[i][j])
+        if (visited[row][col])
             return false;
-        if (board[i][j] != word.charAt(index))
+        if (board[row][col] != word.charAt(index))
             return false;
-        visited[i][j] = true;
-        if (dfs(board, i + 1, j, word, index + 1, visited) ||
-            dfs(board, i - 1, j, word, index + 1, visited) ||
-            dfs(board, i, j + 1, word, index + 1, visited) ||
-            dfs(board, i, j - 1, word, index + 1, visited))
-            return true;
-        return false;
+        boolean found = false;
+        visited[row][col] = true;
+        for (int[] dir : dirs) {
+            if (dfs(board, word, row + dir[0], col + dir[1], index + 1, visited))
+                found = true;
+        }
+        visited[row][col] = false;
+        return found;
     }
 }
