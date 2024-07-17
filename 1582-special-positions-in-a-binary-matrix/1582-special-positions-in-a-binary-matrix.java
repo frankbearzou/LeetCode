75dfc9eb249e8b0d56442e38a0586e283429a1fa
Frank Zou <frankbearzou@gmail.com>
2024-07-16 23:37:23 -0400

    Time: 2 ms (91.84%), Space: 44.9 MB (34.01%) - LeetHub

--- 1582-special-positions-in-a-binary-matrix/1582-special-positions-in-a-binary-matrix.java
+++ 1582-special-positions-in-a-binary-matrix/1582-special-positions-in-a-binary-matrix.java
@@ -1,28 +1,24 @@
 class Solution {
     public int numSpecial(int[][] mat) {
         int count = 0;
+        int[] row = new int[mat.length];
+        int[] col = new int[mat[0].length];
         for (int i = 0; i < mat.length; i++) {
             for (int j = 0; j < mat[0].length; j++) {
-                if (mat[i][j] == 1 && isSpecial(mat, i, j))
+                row[i] += mat[i][j];
+            }
+        }
+        for (int j = 0; j < mat[0].length; j++) {
+            for (int i = 0; i < mat.length; i++) {
+                col[j] += mat[i][j];
+            }
+        }
+        for (int i = 0; i < mat.length; i++) {
+            for (int j = 0; j < mat[0].length; j++) {
+                if (mat[i][j] == 1 && row[i] == 1 && col[j] == 1)
                     count++;
             }
         }
         return count;
-    }
-
-    boolean isSpecial(int[][] mat, int r, int c) {
-        for (int i = 0; i < mat.length; i++) {
-            if (i == r)
-                continue;
-            if (mat[i][c] == 1)
-                return false;
-        }
-        for (int j = 0; j < mat[0].length; j++) {
-            if (j == c)
-                continue;
-            if (mat[r][j] == 1)
-                return false;
-        }
-        return true;
     }
 }
