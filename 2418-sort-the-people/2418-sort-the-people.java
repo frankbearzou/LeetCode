88662ac5a1b51e534894342132a328d2010d3e63
Frank Zou <frankbearzou@gmail.com>
2024-07-21 20:47:05 -0400

    Time: 7 ms (90.64%), Space: 45 MB (91.7%) - LeetHub

--- 2418-sort-the-people/2418-sort-the-people.java
+++ 2418-sort-the-people/2418-sort-the-people.java
@@ -1,12 +1,24 @@
 class Solution {
     public String[] sortPeople(String[] names, int[] heights) {
-        Map<Integer, String> map = new TreeMap<>((a, b) -> b - a);
         int n = names.length;
-        for (int i = 0; i < n; i++)
-            map.put(heights[i], names[i]);
-        int i = 0;
-        for (int key : map.keySet())
-            names[i++] = map.get(key);
-        return names;
+        People[] people = new People[n];
+        for (int i = 0; i < n; i++) {
+            people[i] = new People(names[i], heights[i]);
+        }
+        Arrays.sort(people, (a, b) -> b.height - a.height);
+        String[] ans = new String[n];
+        for (int i = 0; i < n; i++) {
+            ans[i] = people[i].name;
+        }
+        return ans;
+    }
+}
+
+class People {
+    String name;
+    int height;
+    People(String name, int height) {
+        this.name = name;
+        this.height = height;
     }
 }
