45c963bc18370ebfc01099354176fd17c930ffa5
Frank Zou <frankbearzou@gmail.com>
2024-07-28 20:57:34 -0400

    Time: 15 ms (88.75%), Space: 42.8 MB (23.49%) - LeetHub

--- 1395-count-number-of-teams/1395-count-number-of-teams.java
+++ 1395-count-number-of-teams/1395-count-number-of-teams.java
@@ -1,23 +1,22 @@
 class Solution {
     public int numTeams(int[] rating) {
         int count = 0;
-        int n = rating.length;
-        int lSmaller = 0, lGreater = 0, rSmaller = 0, rGreater = 0;
-        for (int i = 1; i < n - 1; i++) {
+        int ls = 0, ll = 0, rs = 0, rl = 0; //l: left, r: right, s:smaller, l: larger
+        for (int i = 1; i < rating.length - 1; i++) {
             for (int j = 0; j < i; j++) {
                 if (rating[j] < rating[i])
-                    lSmaller++;
+                    ls++;
                 else if (rating[j] > rating[i])
-                    lGreater++;
+                    ll++;
             }
-            for (int j = i + 1; j < n; j++) {
+            for (int j = i + 1; j < rating.length; j++) {
                 if (rating[j] < rating[i])
-                    rSmaller++;
+                    rs++;
                 else if (rating[j] > rating[i])
-                    rGreater++;
+                    rl++;
             }
-            count += lSmaller * rGreater + lGreater * rSmaller;
-            lSmaller = lGreater = rSmaller = rGreater = 0;
+            count += ls * rl + ll * rs;
+            ls = ll = rs = rl = 0;
         }
         return count;
     }
