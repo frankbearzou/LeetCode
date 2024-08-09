af81189d23f46da1a4c12e4fb186c050b436c300
Frank Zou <frankbearzou@gmail.com>
2024-08-09 19:29:47 -0400

    Time: 1 ms (97.73%), Space: 42.3 MB (31.62%) - LeetHub

--- 1796-second-largest-digit-in-a-string/1796-second-largest-digit-in-a-string.java
+++ 1796-second-largest-digit-in-a-string/1796-second-largest-digit-in-a-string.java
@@ -1,21 +1,17 @@
 class Solution {
     public int secondHighest(String s) {
-        int[] map = new int[10];
+        int largest = -1, larger = -1;
         for (char c : s.toCharArray()) {
             if (c >= '0' && c <= '9') {
-                map[c - '0']++;
-            }
-        }
-        int largest = -1;
-        for (int i = 9; i >= 0; i--) {
-            if (map[i] != 0) {
-                if (largest == -1) {
-                    largest = map[i];
-                } else {
-                    return map[i];
+                int d = c - '0';
+                if (d > largest) {
+                    larger = largest;
+                    largest = d;
+                } else if (d != largest && d > larger) {
+                    larger = d;
                 }
             }
         }
-        return -1;
+        return larger;
     }
 }
