e7d8eb3126b8141a89932db1dd15bc2d905f7539
Frank Zou <frankbearzou@gmail.com>
2024-08-17 14:00:41 -0400

    Time: 0 ms (100%), Space: 41.7 MB (29.01%) - LeetHub

--- 0791-custom-sort-string/0791-custom-sort-string.java
+++ 0791-custom-sort-string/0791-custom-sort-string.java
@@ -1,22 +1,20 @@
 class Solution {
     public String customSortString(String order, String s) {
-        StringBuilder ans = new StringBuilder();
+        StringBuilder res = new StringBuilder();
         int[] map = new int[26];
-        for (char c : s.toCharArray()) {
+        for (char c : s.toCharArray())
             map[c - 'a']++;
-        }
         for (char c : order.toCharArray()) {
-            while (map[c - 'a'] > 0) {
-                ans.append(c);
-                map[c - 'a']--;
+            int index = c - 'a';
+            while (map[index]-- > 0) {
+                res.append(c);
             }
         }
         for (int i = 0; i < 26; i++) {
-            while (map[i] > 0) {
-                ans.append((char)('a' + i));
-                map[i]--;
+            while (map[i]-- > 0) {
+                res.append((char)('a' + i));
             }
         }
-        return ans.toString();
+        return res.toString();
     }
 }
