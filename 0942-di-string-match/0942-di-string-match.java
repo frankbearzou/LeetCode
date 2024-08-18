81e9d2eb94b6b9d9b5ca533a66ad3d4cb2f0cb2f
Frank Zou <frankbearzou@gmail.com>
2024-08-18 16:10:11 -0400

    Time: 2 ms (98.28%), Space: 45.2 MB (15.46%) - LeetHub

---
+++ 0942-di-string-match/0942-di-string-match.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[] diStringMatch(String s) {
+        int n = s.length();
+        int[] res = new int[n + 1];
+        int l = 0, r = n;
+        for (int i = 0; i < n; i++) {
+            if (s.charAt(i) == 'I') {
+                res[i] = l++;
+            } else {
+                res[i] = r--;
+            }
+        }
+        res[n] = l;
+        return res;
+    }
+}
