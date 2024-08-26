f220c4cc422993aeef158ea03cd55335f0113699
Frank Zou <frankbearzou@gmail.com>
2024-08-26 09:18:36 -0400

    Time: 0 ms (100%), Space: 51.1 MB (61.72%) - LeetHub

--- 0938-range-sum-of-bst/0938-range-sum-of-bst.java
+++ 0938-range-sum-of-bst/0938-range-sum-of-bst.java
@@ -14,15 +14,13 @@
  * }
  */
 class Solution {
-    int sum = 0;
     public int rangeSumBST(TreeNode root, int low, int high) {
         if (root == null)
             return 0;
-        if (low <= root.val && root.val <= high) {
-            sum += root.val;
-        }
-        rangeSumBST(root.left, low, high);
-        rangeSumBST(root.right, low, high);
-        return sum;
+        if (root.val < low)
+            return rangeSumBST(root.right, low, high);
+        if (root.val > high)
+            return rangeSumBST(root.left, low, high);
+        return root.val + rangeSumBST(root.left, low, high) + rangeSumBST(root.right, low, high);
     }
 }
