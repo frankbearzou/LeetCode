9e8eab0be8ea424a9ad7c00e8cc794c1d293fba4
Frank Zou <frankbearzou@gmail.com>
2024-08-27 13:32:51 -0400

    Time: 1 ms (85.92%), Space: 44.9 MB (32.19%) - LeetHub

--- 1110-delete-nodes-and-return-forest/1110-delete-nodes-and-return-forest.java
+++ 1110-delete-nodes-and-return-forest/1110-delete-nodes-and-return-forest.java
@@ -18,28 +18,24 @@ class Solution {
     Set<Integer> set = new HashSet<>();
 
     public List<TreeNode> delNodes(TreeNode root, int[] to_delete) {
-        for (int i : to_delete) {
+        for (int i : to_delete)
             set.add(i);
-        }
-        root = dfs(root);
-        if (root != null) {
+        if (build(root) != null) {
             res.add(root);
         }
         return res;
     }
 
-    TreeNode dfs(TreeNode root) {
+    TreeNode build(TreeNode root) {
         if (root == null)
-            return root;
-        root.left = dfs(root.left);
-        root.right = dfs(root.right);
+            return null;
+        root.left = build(root.left);
+        root.right = build(root.right);
         if (set.contains(root.val)) {
-            if (root.left != null) {
+            if (root.left != null)
                 res.add(root.left);
-            }
-            if (root.right != null) {
+            if (root.right != null)
                 res.add(root.right);
-            }
             return null;
         }
         return root;
