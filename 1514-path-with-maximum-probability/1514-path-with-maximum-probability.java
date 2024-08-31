f22da8278c880198bb8e3bacaf57bba1010ab110
Frank Zou <frankbearzou@gmail.com>
2024-08-30 23:37:29 -0400

    Time: 10 ms (92.38%), Space: 55.8 MB (85.72%) - LeetHub

--- 1514-path-with-maximum-probability/1514-path-with-maximum-probability.java
+++ 1514-path-with-maximum-probability/1514-path-with-maximum-probability.java
@@ -1,26 +1,25 @@
 class Solution {
     public double maxProbability(int n, int[][] edges, double[] succProb, int start_node, int end_node) {
-        double[] maxProb = new double[n];
-        maxProb[start_node] = 1;
-        // bellman ford algorithm
+        double[] res = new double[n];
+        res[start_node] = 1;
         for (int i = 0; i < n - 1; i++) {
             boolean changed = false;
             for (int j = 0; j < edges.length; j++) {
                 int u = edges[j][0];
                 int v = edges[j][1];
-                double prob = succProb[j];
-                if (maxProb[u] * prob > maxProb[v]) {
-                    maxProb[v] = maxProb[u] * prob;
+                double p = succProb[j];
+                if (res[u] * p > res[v]) {
+                    res[v] = res[u] * p;
                     changed = true;
                 }
-                if (maxProb[v] * prob > maxProb[u]) {
-                    maxProb[u] = maxProb[v] * prob;
+                if (res[v] * p > res[u]) {
+                    res[u] = res[v] * p;
                     changed = true;
                 }
             }
             if (!changed)
                 break;
         }
-        return maxProb[end_node];
+        return res[end_node];
     }
 }
