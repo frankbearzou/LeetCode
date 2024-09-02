33232078d2b61869d58871efff2efe2945a0f1b9
Frank Zou <frankbearzou@gmail.com>
2024-09-01 20:46:26 -0400

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1894-find-the-student-that-will-replace-the-chalk/1894-find-the-student-that-will-replace-the-chalk.java
+++ 1894-find-the-student-that-will-replace-the-chalk/1894-find-the-student-that-will-replace-the-chalk.java
@@ -1,19 +1,14 @@
 class Solution {
     public int chalkReplacer(int[] chalk, int k) {
-        int i = 0;
-        int n = chalk.length;
         int sum = 0;
-        for (int c : chalk)
-            sum += c;
+        for (int i : chalk)
+            sum += i;
         k = k % sum;
-        while (k >= 0) {
-            if (chalk[i] > k)
+        for (int i = 0; i < chalk.length; i++) {
+            if (k < chalk[i])
                 return i;
             k -= chalk[i];
-            i++;
-            if (i % n == 0)
-                i = 0;
         }
-        return i;
+        return 0;
     }
 }
