a9ead6c5c4e75b55805e81e863500d470628d0dd
Frank Zou <frankbearzou@gmail.com>
2024-09-07 10:25:32 -0400

    Time: 0 ms (100%), Space: 41.9 MB (71.79%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
@@ -1,23 +1,28 @@
 class Solution {
-    String[] letters = {"abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+    int n;
     List<String> ans = new ArrayList<>();
+    char[] digits;
+    char[] path;
+    String[] map = {"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
 
     public List<String> letterCombinations(String digits) {
-        if (digits.length() == 0)
+        n = digits.length();
+        if (n == 0)
             return ans;
-        backtracking(digits, 0, "");
+        this.digits = digits.toCharArray();
+        path = new char[n];
+        dfs(0);
         return ans;
     }
 
-    void backtracking(String digits, int start, String path) {
-        if (path.length() == digits.length()) {
+    void dfs(int i) {
+        if (i == n) {
             ans.add(new String(path));
             return;
         }
-        for (int i = start; i < digits.length(); i++) {
-            for (char c : letters[digits.charAt(i) - '2'].toCharArray()) {
-                backtracking(digits, i + 1, path + c);
-            }
+        for (char c : map[digits[i] - '0'].toCharArray()) {
+            path[i] = c;
+            dfs(i + 1);
         }
     }
 }
