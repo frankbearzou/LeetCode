54029b4f39fa227c2e7b4753de9d4c4a25fe18f9
Frank Zou <frankbearzou@gmail.com>
2024-09-08 20:49:02 -0400

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 2326-spiral-matrix-iv/2326-spiral-matrix-iv.java
@@ -0,0 +1,78 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    int[][] matrix;
+    ListNode head;
+
+    public int[][] spiralMatrix(int m, int n, ListNode head) {
+        matrix = new int[m][n];
+        if (m == 1 && n == 1) {
+            matrix[0][0] = head.val;
+            return matrix;
+        }
+        this.head = head;
+        int left = 0, right = n - 1, top = 0, down = m - 1;
+        for (int i = 0; i < m; i++)
+            Arrays.fill(matrix[i], -1);
+
+        circle(left, right, top, down);
+        return matrix;
+    }
+
+    void circle(int left, int right, int top, int down) {
+        while (left < right && top < down) {
+            for (int j = left; j < right; j++) {
+                if (head != null) {
+                    matrix[top][j] = head.val;
+                    head = head.next;
+                }
+            }
+            for (int i = top; i < down; i++) {
+                if (head != null) {
+                    matrix[i][right] = head.val;
+                    head = head.next;
+                }
+            }
+            for (int j = right; j > left; j--) {
+                if (head != null) {
+                    matrix[down][j] = head.val;
+                    head = head.next;
+                }
+            }
+            for (int i = down; i > top; i--) {
+                if (head != null) {
+                    matrix[i][left] = head.val;
+                    head = head.next;
+                }
+            }
+            left++;
+            right--;
+            top++;
+            down--;
+        }
+        if (left < right) {
+            for (int j = left; j <= right; j++) {
+                if (head != null) {
+                    matrix[top][j] = head.val;
+                    head = head.next;
+                }
+            }
+        }
+        if (top < down) {
+            for (int i = top; i <= down; i++) {
+                if (head != null) {
+                    matrix[i][left] = head.val;
+                    head = head.next;
+                }
+            }
+        }
+    }
+}
