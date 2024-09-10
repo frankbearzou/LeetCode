2d71fb39fa8d9f0c024d1443a36ac1b432c48f25
Frank Zou <frankbearzou@gmail.com>
2024-09-10 15:12:22 -0400

    Time: 7 ms (98.14%), Space: 57.2 MB (22.96%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.java
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.java
@@ -1,31 +1,36 @@
 class Solution {
+    List<List<String>> res = new ArrayList<>();
+    List<String> path = new ArrayList<>();
+    String s;
+    int n;
+
     public List<List<String>> partition(String s) {
-        List<List<String>> res = new ArrayList<>();
-        List<String> path = new ArrayList<>();
-        backtracking(s, 0, path, res);
+        this.s = s;
+        n = s.length();
+        dfs(0);
         return res;
     }
 
-    void backtracking(String s, int start, List<String> path, List<List<String>> res) {
-        if (start == s.length()) {
+    void dfs(int start) {
+        if (start == n) {
             res.add(new ArrayList(path));
             return;
         }
-        for (int i = start; i < s.length(); i++) {
-            if (!isPalindrome(s, start, i))
-                continue;
-            path.add(s.substring(start, i + 1));
-            backtracking(s, i + 1, path, res);
-            path.remove(path.size() - 1);
+        for (int i = start; i < n; i++) {
+            if (isPalindrome(start, i)) {
+                path.add(s.substring(start, i + 1));
+                dfs(i + 1);
+                path.removeLast();
+            }
         }
     }
 
-    boolean isPalindrome(String s, int start, int end) {
-        while (start < end) {
-            if (s.charAt(start) != s.charAt(end))
+    boolean isPalindrome(int l, int r) {
+        while (l < r) {
+            if (s.charAt(l) != s.charAt(r))
                 return false;
-            start++;
-            end--;
+            l++;
+            r--;
         }
         return true;
     }
