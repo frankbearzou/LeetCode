bcc2b747e197e80a3151626c792f2757f7d93c12
Frank Zou <frankbearzou@gmail.com>
2024-09-11 11:24:47 -0400

    Time: 0 ms (100%), Space: 40.7 MB (78.33%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,13 +1,22 @@
 class Solution {
+    int[] nums;
+    int[] dp;
+
     public int rob(int[] nums) {
+        this.nums = nums;
         int n = nums.length;
-        if (n == 1)
-            return nums[0];
-        int[] dp = new int[n + 1];
-        dp[1] = nums[0];
-        for (int i = 2; i <= n; i++) {
-            dp[i] = Math.max(dp[i - 1], nums[i - 1] + dp[i - 2]);
-        }
-        return dp[n];
+        dp = new int[n];
+        Arrays.fill(dp, -1);
+        dfs(n - 1);
+        return dp[n - 1];
+    }
+
+    int dfs(int i) {
+        if (i < 0)
+            return 0;
+        if (dp[i] != -1)
+            return dp[i];
+        dp[i] = Math.max(nums[i] + dfs(i - 2), dfs(i - 1));
+        return dp[i];
     }
 }
