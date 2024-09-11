f7b49b885c5be68a57bc6b6b061ab7d57baaea23
Frank Zou <frankbearzou@gmail.com>
2024-09-11 13:15:23 -0400

    Time: 0 ms (100%), Space: 40.4 MB (39.05%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,13 +1,22 @@
 class Solution {
+    int[] cache;
+
     public int climbStairs(int n) {
-        if (n < 3)
+        if (n <= 2)
             return n;
-        int a = 1, b = 2, ans = 0;
-        for (int i = 3; i <= n; i++) {
-            ans = a + b;
-            a = b;
-            b = ans;
-        }
-        return ans;
+        cache = new int[n + 1];
+        Arrays.fill(cache, -1);
+        cache[1] = 1;
+        cache[2] = 2;
+        return dfs(n);
+    }
+
+    int dfs(int n) {
+        if (n <= 0)
+            return 0;
+        if (cache[n] != -1)
+            return cache[n];
+        cache[n] = dfs(n - 1) + dfs(n - 2);
+        return cache[n];
     }
 }
