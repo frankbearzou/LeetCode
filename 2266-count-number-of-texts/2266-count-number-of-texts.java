dd4ae690b67c19377ff1d89922d6cbfb148089fa
Frank Zou <frankbearzou@gmail.com>
2024-09-14 14:27:03 -0400

    Time: 14 ms (94.2%), Space: 45.5 MB (57.14%) - LeetHub

--- 2266-count-number-of-texts/2266-count-number-of-texts.java
+++ 2266-count-number-of-texts/2266-count-number-of-texts.java
@@ -1,21 +1,24 @@
 class Solution {
-    private static final int mod = (int)1e9 + 7;
-    private static final int len = (int)1e5 + 1;
-    private static final long[] f = new long[len];
-    private static final long[] g = new long[len];
+    public int countTexts(String pressedKeys) {
+        int len = pressedKeys.length() + 1;
+        if (len < 3)
+            return 1;
+        
 
-    static {
+        int mod = (int)1e9 + 7;
+        
+        long[] f = new long[len];
+        long[] g = new long[len];
+
         f[0] = g[0] = 1;
         f[1] = g[1] = 1;
         f[2] = g[2] = 2;
         f[3] = g[3] = 4;
         for (int i = 4; i < len; i++) {
-            f[i] = f[i - 1] + f[i - 2] + f[i - 3];
-            g[i] = g[i - 1] + g[i - 2] + g[i - 3] + g[i - 4];
+            f[i] = (f[i - 1] + f[i - 2] + f[i - 3]) % mod;
+            g[i] = (g[i - 1] + g[i - 2] + g[i - 3] + g[i - 4]) % mod;
         }
-    }
 
-    public int countTexts(String pressedKeys) {
         long ans = 1;
         int count = 0;
         for (int i = 0; i < pressedKeys.length(); i++) {
