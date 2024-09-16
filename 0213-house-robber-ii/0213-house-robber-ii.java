2145dd5e793ea4fca3fde6e8c58b8951901accd8
Frank Zou <frankbearzou@gmail.com>
2024-09-15 22:23:48 -0400

    Time: 0 ms (100%), Space: 41.3 MB (12.02%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.java
+++ 0213-house-robber-ii/0213-house-robber-ii.java
@@ -3,17 +3,21 @@ class Solution {
         int n = nums.length;
         if (n == 1)
             return nums[0];
-        return Math.max(rob(nums, 0, n - 2), rob(nums, 1, n - 1));
+        return Math.max(robs(Arrays.copyOfRange(nums, 0, n - 1)), robs(Arrays.copyOfRange(nums, 1, n)));
     }
 
-    int rob(int[] nums, int start, int end) {
-        if (end - start == 0)
-            return nums[start];
-        int[] dp = new int[end - start + 2];
-        dp[1] = nums[start];
-        for (int i = 2; i < dp.length; i++) {
-            dp[i] = Math.max(dp[i - 1], nums[i + start - 1] + dp[i - 2]);
+    int robs(int[] nums) {
+        int n = nums.length;
+        if (n == 1)
+            return nums[0];
+        if (n == 2)
+            return Math.max(nums[0], nums[1]);
+        int[] dp = new int[n];
+        dp[0] = nums[0];
+        dp[1] = Math.max(nums[0], nums[1]);
+        for (int i = 2; i < n; i++) {
+            dp[i] = Math.max(nums[i] + dp[i - 2], dp[i - 1]);
         }
-        return dp[dp.length - 1];
+        return dp[n - 1];
     }
 }
