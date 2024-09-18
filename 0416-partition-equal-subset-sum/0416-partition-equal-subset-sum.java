df516eff091bade202f8a938279773e12c3ff78f
Frank Zou <frankbearzou@gmail.com>
2024-09-18 14:41:48 -0400

    Time: 40 ms (67.07%), Space: 47 MB (47.39%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
@@ -1,18 +1,24 @@
 class Solution {
     public boolean canPartition(int[] nums) {
+        int n = nums.length;
         int sum = 0;
-        for (int num : nums) {
+        for (int num : nums)
             sum += num;
-        }
-        if ((sum & 1) == 1)
+        if ((sum & 1) == 1) // odd
             return false;
         int target = sum / 2;
-        int[] dp = new int[target + 1];
-        for (int i = 0; i < nums.length; i++) {
-            for (int j = target; j >= nums[i]; j--) {
-                dp[j] = Math.max(dp[j], dp[j - nums[i]] + nums[i]);
+        boolean[][] dp = new boolean[n + 1][target + 1];
+        for (int i = 0; i <= n; i++) {
+            dp[i][0] = true;
+        }
+        for (int i = 1; i <= n; i++) {
+            for (int j = 0; j <= target; j++) {
+                if (j < nums[i - 1])
+                    dp[i][j] = dp[i - 1][j];
+                else
+                    dp[i][j] = dp[i - 1][j] || dp[i - 1][j - nums[i - 1]];
             }
         }
-        return dp[target] == target;
+        return dp[n][target];
     }
 }
