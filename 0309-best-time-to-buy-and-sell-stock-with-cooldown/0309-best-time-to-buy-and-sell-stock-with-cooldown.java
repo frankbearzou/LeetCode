e96cfd252ebd47e931343b79e1de9027fc720df2
Frank Zou <frankbearzou@gmail.com>
2024-09-27 16:20:00 -0400

    Time: 1 ms (85.14%), Space: 41.6 MB (71.22%) - LeetHub

--- 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.java
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.java
@@ -1,24 +1,18 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        //state
-        //0: hold stock
-        //1: sold stock
-        //2: sell today
-        //3: cooldown
-        int[][] dp = new int[prices.length][4];
-        dp[0][0] = -prices[0];
-        for (int i = 1; i < prices.length; i++) {
-            for (int j = 0; j < 4; j++) {
-                dp[i][0] = Math.max(dp[i - 1][0], Math.max(dp[i - 1][1] - prices[i], dp[i - 1][3] - prices[i]));
-                dp[i][1] = Math.max(dp[i - 1][1], dp[i - 1][3]);
-                dp[i][2] = dp[i - 1][0] + prices[i];
-                dp[i][3] = dp[i - 1][2];
-            }
+        int n = prices.length;
+        if (n == 1)
+            return 0;
+        int[][] dp = new int[n][2];
+        dp[0][1] = -prices[0];
+        dp[1][0] = Math.max(0, prices[1] - prices[0]);
+        dp[1][1] = Math.max(-prices[0], -prices[1]);
+        for (int i = 2; i < n; i++) {
+            // not hold
+            dp[i][0] = Math.max(dp[i - 1][0], dp[i - 1][1] + prices[i]);
+            // hold
+            dp[i][1] = Math.max(dp[i - 1][1], dp[i - 2][0] - prices[i]);
         }
-        int max = dp[dp.length - 1][0];
-        for (int i = 1; i < 4; i++) {
-            max = Math.max(max, dp[dp.length - 1][i]);
-        }
-        return max;
+        return dp[n - 1][0];
     }
 }
