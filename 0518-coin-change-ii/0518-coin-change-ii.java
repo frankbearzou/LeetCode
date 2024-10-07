427e7ab04cbba28448f8f733e22634c3f8a856cc
Frank Zou <frankbearzou@gmail.com>
2024-10-07 15:24:17 -0400

    Time: 20 ms (9.81%), Space: 53.9 MB (5.18%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.java
+++ 0518-coin-change-ii/0518-coin-change-ii.java
@@ -1,17 +1,28 @@
 class Solution {
+    int[] coins;
+    int n;
+    Integer[][] cache;
+
     public int change(int amount, int[] coins) {
-        int n = coins.length;
-        int[][] dp = new int[n + 1][amount + 1];
-        for (int i = 0; i <= n; i++)
-            dp[i][0] = 1;
-        for (int i = 1; i <= n; i++) {
-            for (int j = 0; j <= amount; j++) {
-                if (j - coins[i - 1] < 0)
-                    dp[i][j] = dp[i - 1][j];
-                else
-                    dp[i][j] = dp[i - 1][j] + dp[i][j - coins[i - 1]];
-            }
-        }
-        return dp[n][amount];
+        this.coins = coins;
+        n = coins.length;
+        cache = new Integer[amount + 1][n];
+        return dfs(amount, 0);
+    }
+
+    int dfs(int amount, int i) {
+        if (amount == 0)
+            return 1;
+        if (i == n)
+            return 0;
+        if (cache[amount][i] != null)
+            return cache[amount][i];
+        int ways = 0;
+        // choose
+        if (amount >= coins[i])
+            ways += dfs(amount - coins[i], i);
+        // not choose
+        ways += dfs(amount, i + 1);
+        return cache[amount][i] = ways;
     }
 }
