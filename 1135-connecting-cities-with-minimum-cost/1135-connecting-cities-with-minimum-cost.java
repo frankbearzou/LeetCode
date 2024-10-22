a482ed50002c666dafee07648d43d470110f8af6
Frank Zou <frankbearzou@gmail.com>
2024-10-22 14:39:49 -0400

    Time: 25 ms (75.62%), Space: 49.4 MB (51.49%) - LeetHub

--- 1135-connecting-cities-with-minimum-cost/1135-connecting-cities-with-minimum-cost.java
+++ 1135-connecting-cities-with-minimum-cost/1135-connecting-cities-with-minimum-cost.java
@@ -1,50 +1,50 @@
 class Solution {
-    public int minimumCost(int n, int[][] connections) {
-        int max = Integer.MAX_VALUE;
-        int[] cost = new int[n + 1];
-        boolean[] tree = new boolean[n + 1];
-        List<int[]>[] dist = new List[n + 1];
+    int n;
+    int[] parent;
 
-        for (int i = 2; i <= n; i++)
-            cost[i] = max;
-        
-        for (int i = 0; i <= n; i++)
-            dist[i] = new ArrayList<>();
+    void init() {
+        parent = new int[n + 1];
+        for (int i = 0; i <= n; i++) {
+            parent[i] = i;
+        }
+    }
+
+    int find(int u) {
+        return (parent[u] == u) ? u : (parent[u] = find(parent[u]));
+    }
+
+    boolean isSame(int u, int v) {
+        u = find(u);
+        v = find(v);
+        return u == v;
+    }
+
+    void union(int u, int v) {
+        u = find(u);
+        v = find(v);
+        if (u == v)
+            return;
+        parent[u] = v;
+    }
+
+    public int minimumCost(int n, int[][] connections) {
+        int ans = 0;
+        int count = 0;
+        this.n = n;
+        init();
+        Arrays.sort(connections, (a, b) -> a[2] - b[2]);
         for (int[] conn : connections) {
             int u = conn[0];
             int v = conn[1];
-            int d = conn[2];
-            dist[u].add(new int[]{v, d});
-            dist[v].add(new int[]{u, d});
+            int w = conn[2];
+            if (isSame(u, v))
+                continue;
+            ans += w;
+            count++;
+            union(u, v);
         }
-
-        // prim
-        for (int i = 0; i < n; i++) {
-            int min = max;
-            int cur = -1;
-            for (int j = 1; j <= n; j++) {
-                if (!tree[j] && cost[j] < min) {
-                    min = cost[j];
-                    cur = j;
-                }
-            }
-            if (cur == -1)
-                break;
-            tree[cur] = true;
-            for (int[] ver : dist[cur]) {
-                int v = ver[0];
-                int d = ver[1];
-                if (!tree[v] && d < cost[v])
-                    cost[v] = d;
-            }
-        }
-
-        int sum = 0;
-        for (int i = 1; i <= n; i++) {
-            if (cost[i] == max)
-                return -1;
-            sum += cost[i];
-        }
-        return sum;
+        if (count != n - 1)
+            return -1;
+        return ans;
     }
 }
