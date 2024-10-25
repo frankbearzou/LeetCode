1758ff1254ec92f4418fb6e7195430ee2b8359d4
Frank Zou <frankbearzou@gmail.com>
2024-10-25 13:37:20 -0400

    Time: 36 ms (36.81%), Space: 58.3 MB (19.35%) - LeetHub

--- 1514-path-with-maximum-probability/1514-path-with-maximum-probability.java
+++ 1514-path-with-maximum-probability/1514-path-with-maximum-probability.java
@@ -1,33 +1,38 @@
 class Solution {
     public double maxProbability(int n, int[][] edges, double[] succProb, int start_node, int end_node) {
-        double[] cost = new double[n];
-        boolean[] visited = new boolean[n];
-        cost[start_node] = 1;
+        List<List<Pair<Integer, Double>>> graph = new ArrayList<>();
         for (int i = 0; i < n; i++) {
-            int cur = -1;
-            double maxCost = 0;
-            for (int j = 0; j < n; j++) {
-                if (visited[j])
-                    continue;
-                if (cost[j] > maxCost) {
-                    cur = j;
-                    maxCost = cost[j];
-                }
-            }
-            if (cur == -1)
-                break;
+            graph.add(new ArrayList<>());
+        }
+        for (int i = 0; i < edges.length; i++) {
+            int u = edges[i][0];
+            int v = edges[i][1];
+            double c = succProb[i];
+            graph.get(u).add(new Pair(v, c));
+            graph.get(v).add(new Pair(u, c));
+        }
+        double[] cost = new double[n];
+        cost[start_node] = 1;
+        boolean[] visited = new boolean[n];
+        PriorityQueue<Pair<Integer, Double>> queue = new PriorityQueue<>((a, b) -> Double.compare(b.getValue(), a.getValue()));
+        queue.add(new Pair(start_node, 1.0));
+        while (!queue.isEmpty()) {
+            Pair<Integer, Double> top = queue.remove();
+            int cur = top.getKey();
+            double c = top.getValue();
+
+            if (cur == end_node)
+                return cost[end_node];
+
             visited[cur] = true;
-            for (int j = 0; j < edges.length; j++) {
-                int u = edges[j][0];
-                int v = edges[j][1];
-                double c = succProb[j];
-                if (u == cur && !visited[v]) {
-                    if (cost[u] * c > cost[v])
-                        cost[v] = cost[u] * c;
-                }
-                if (v == cur && !visited[u]) {
-                    if (cost[v] * c > cost[u])
-                        cost[u] = cost[v] * c;
+            for (Pair<Integer, Double> vec : graph.get(cur)) {
+                int to = vec.getKey();
+                double toCost = vec.getValue();
+                if (visited[to])
+                    continue;
+                if (c * toCost > cost[to]) {
+                    cost[to] = c * toCost;
+                    queue.add(new Pair(to, cost[to]));
                 }
             }
         }
