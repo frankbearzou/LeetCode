19ea889bc8e8083842bea172cc6171eed6a6a40d
Frank Zou <frankbearzou@gmail.com>
2024-10-29 23:05:46 -0400

    Time: 3 ms (69.81%), Space: 44.9 MB (6.45%) - LeetHub

--- 0265-paint-house-ii/0265-paint-house-ii.java
+++ 0265-paint-house-ii/0265-paint-house-ii.java
@@ -1,27 +1,31 @@
 class Solution {
+    int[][] costs;
+    int n;
+    int k;
+    Integer[][] cache;
+
     public int minCostII(int[][] costs) {
-        int n = costs.length;
-        int m = costs[0].length;
-        for (int i = 1; i < n; i++) {
-            int min = -1, secondMin = -1;
-            for (int j = 0; j < m; j++) {
-                if (min == -1 || costs[i - 1][j] <= costs[i - 1][min]) {
-                    secondMin = min;
-                    min = j;
-                } else if (secondMin == -1 || costs[i - 1][j] < costs[i - 1][secondMin]) {
-                    secondMin = j;
-                }
-            }
-            for (int j = 0; j < m; j++) {
-                if (j != min)
-                    costs[i][j] += costs[i - 1][min];
-                else
-                    costs[i][j] += costs[i - 1][secondMin];
-            }
+        this.costs = costs;
+        n = costs.length;
+        k = costs[0].length;
+        cache = new Integer[n][k];
+        int ans = Integer.MAX_VALUE;
+        for (int c = 0; c < k; c++)
+            ans = Math.min(ans, dfs(0, c));
+        return ans;
+    }
+
+    int dfs(int index, int color) {
+        if (index == n)
+            return 0;
+        if (cache[index][color] != null)
+            return cache[index][color];
+        int sum = Integer.MAX_VALUE;
+        for (int c = 0; c < k; c++) {
+            if (c == color)
+                continue;
+            sum = Math.min(sum, costs[index][c] + dfs(index + 1, c));
         }
-        int min = costs[n - 1][0];
-        for (int j = 1; j < m; j++)
-            min = Math.min(min, costs[n - 1][j]);
-        return min;
+        return cache[index][color] = sum;
     }
 }
