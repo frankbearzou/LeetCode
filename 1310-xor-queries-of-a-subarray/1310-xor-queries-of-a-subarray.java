cdcd42dad8a9197d27b8a2130b61d78d39378017
Frank Zou <frankbearzou@gmail.com>
2024-11-23 15:43:28 -0500

    Time: 3 ms (59.51%), Space: 60.8 MB (30.28%) - LeetHub

--- 1310-xor-queries-of-a-subarray/1310-xor-queries-of-a-subarray.java
+++ 1310-xor-queries-of-a-subarray/1310-xor-queries-of-a-subarray.java
@@ -1,19 +1,20 @@
 class Solution {
     public int[] xorQueries(int[] arr, int[][] queries) {
-        // prefix xor, running xor
-        int n = queries.length;
+        int n = arr.length;
+        int[] ans = new int[queries.length];
         int[] prefix = new int[n];
         prefix[0] = arr[0];
-        for (int i = 1; i < arr.length; i++) {
-            prefix[i] = prefix[i - 1] ^ arr[i];
+        for (int i = 1; i < n; i++) {
+            prefix[i] = arr[i] ^ prefix[i - 1];
         }
-        int[] ans = new int[n];
-        for (int i = 0; i < n; i++) {
-            int left = queries[i][0];
-            int right = queries[i][1];
-            ans[i] = prefix[right];
-            if (left > 0)
-                ans[i] = ans[i] ^ prefix[left - 1];
+        for (int i = 0; i < queries.length; i++) {
+            int l = queries[i][0];
+            int r = queries[i][1];
+            if (l == 0) {
+                ans[i] = prefix[r];
+            } else {
+                ans[i] = prefix[l - 1] ^ prefix[r];
+            }
         }
         return ans;
     }
