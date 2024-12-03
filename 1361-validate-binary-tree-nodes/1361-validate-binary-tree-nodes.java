4aadbb1df483f323466c232faad4af9e93c1bc33
Frank Zou <frankbearzou@gmail.com>
2024-12-02 23:01:30 -0500

    Time: 28 ms (11.96%), Space: 46 MB (43.72%) - LeetHub

--- 1361-validate-binary-tree-nodes/1361-validate-binary-tree-nodes.java
+++ 1361-validate-binary-tree-nodes/1361-validate-binary-tree-nodes.java
@@ -1,53 +1,42 @@
 class Solution {
-    int[] parents;
-    int[] rank;
-
-    void init(int n) {
-        parents = new int[n];
-        rank = new int[n];
-        for (int i = 0; i < n; i++) {
-            parents[i] = i;
-            rank[i] = 1;
-        }
-    }
-
-    int find(int u) {
-        return (parents[u] == u) ? u : (parents[u] = find(parents[u]));
-    }
-
-    int union(int u, int v) {
-        u = find(u);
-        v = find(v);
-        if (u == v)
-            return 0;
-        if (rank[u] < rank[v])
-            parents[u] = v;
-        else if (rank[v] < rank[u])
-            parents[v] = u;
-        else {
-            parents[v] = u;
-            u++;
-        }
-        return 1;
-    }
-
     public boolean validateBinaryTreeNodes(int n, int[] leftChild, int[] rightChild) {
-        init(n);
-        int count = 0;
-        for (int i = 0; i < leftChild.length; i++) {
-            if (leftChild[i] != -1) {
-                if (union(i, leftChild[i]) == 0)
+        int root = findRoot(n, leftChild, rightChild);
+        if (root == -1)
+            return false;
+        Queue<Integer> queue = new ArrayDeque<>();
+        Set<Integer> visited = new HashSet<>();
+        queue.add(root);
+        visited.add(root);
+        while (!queue.isEmpty()) {
+            int node = queue.remove();
+            int l = leftChild[node];
+            int r = rightChild[node];
+            if (l != -1) {
+                if (visited.contains(l))
                     return false;
-                else
-                    count++;
+                queue.add(l);
+                visited.add(l);
             }
-            if (rightChild[i] != -1) {
-                if (union(i, rightChild[i]) == 0)
+            if (r != -1) {
+                if (visited.contains(r))
                     return false;
-                else
-                    count++;
+                queue.add(r);
+                visited.add(r);
             }
         }
-        return count == n - 1;
+        return visited.size() == n;
+    }
+
+    int findRoot(int n, int[] leftChild, int[] rightChild) {
+        Set<Integer> set = new HashSet<>();
+        for (int i = 0; i < leftChild.length; i++) {
+            set.add(leftChild[i]);
+            set.add(rightChild[i]);
+        }
+        for (int i = 0; i < n; i++) {
+            if (!set.contains(i))
+                return i;
+        }
+        return -1;
     }
 }
