bad9da837f3285888c59d7c885e9b21bf0336130
Frank Zou <frankbearzou@gmail.com>
2024-12-05 23:03:46 -0500

    Time: 23 ms (61.22%), Space: 45.7 MB (8.16%) - LeetHub

--- 1390-four-divisors/1390-four-divisors.java
+++ 1390-four-divisors/1390-four-divisors.java
@@ -1,34 +1,22 @@
 class Solution {
-    Map<Integer, Integer> map = new HashMap<>();
-
     public int sumFourDivisors(int[] nums) {
         int sum = 0;
         for (int num : nums) {
-            sum += getSum(num);
+            Set<Integer> set = new HashSet<>();
+            for (int i = 2; i * i <= num; i++) {
+                if (num % i == 0) {
+                    set.add(i);
+                    set.add(num / i);
+                }
+                if (set.size() > 2)
+                    break;
+            }
+            if (set.size() == 2) {
+                sum += 1 + num;
+                for (int s : set)
+                    sum += s;
+            }
         }
         return sum;
-    }
-
-    int getSum(int num) {
-        if (map.containsKey(num))
-            return map.get(num);
-        int count = 2;
-        int[] arr = new int[3];
-        for (int i = 2; i <= num / 2; i++) {
-            if (num % i == 0) {
-                arr[count - 2] = i;
-                count++;
-            }
-            if (count > 4) {
-                count = 0;
-                break;
-            }
-        }
-        if (count == 4) {
-            map.put(num, 1 + arr[0] + arr[1] + num);
-        } else {
-            map.put(num, 0);
-        }
-        return map.get(num);
     }
 }
