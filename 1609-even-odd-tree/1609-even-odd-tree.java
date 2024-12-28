5d4f564c6ccf12fdca279a5a5861da9f184bb135
Frank Zou <frankbearzou@gmail.com>
2024-12-28 17:43:21 -0500

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1609-even-odd-tree/1609-even-odd-tree.java
+++ 1609-even-odd-tree/1609-even-odd-tree.java
@@ -15,37 +15,36 @@
  */
 class Solution {
     public boolean isEvenOddTree(TreeNode root) {
+        Queue<TreeNode> queue = new ArrayDeque<>();
+        queue.add(root);
         int level = 0;
-        Queue<TreeNode> queue = new LinkedList<>();
-        if (root != null) {
-            queue.offer(root);
-        }
         while (!queue.isEmpty()) {
             int size = queue.size();
             int pre = 0;
             for (int i = 0; i < size; i++) {
-                TreeNode node = queue.poll();
-                if (node.left != null)
-                    queue.offer(node.left);
-                if (node.right != null)
-                    queue.offer(node.right);
-                int val = node.val;
-                if (level % 2 == 0 && val % 2 == 0)
-                    return false;
-                if (level % 2 == 1 && val % 2 == 1)
-                    return false;
+                TreeNode node = queue.remove();
+                if (level % 2 == 0) {
+                    if (node.val % 2 == 0)
+                        return false;
+                } else {
+                    if (node.val % 2 == 1)
+                        return false;
+                }
                 if (i == 0) {
-                    pre = val;
+                    pre = node.val;
                 } else {
                     if (level % 2 == 0) {
-                        if (pre >= val)
+                        if (pre >= node.val)
                             return false;
                     } else {
-                        if (pre <= val)
+                        if (pre <= node.val)
                             return false;
                     }
-                    pre = val;
                 }
+                if (node.left != null)
+                    queue.add(node.left);
+                if (node.right != null)
+                    queue.add(node.right);
             }
             level++;
         }
