3d2889691f848c6c9b2fbdacfc0537aacff545d1
Frank Zou <frankbearzou@gmail.com>
2024-12-29 17:23:15 -0500

    Time: 3 ms (95.71%), Space: 73.1 MB (29.86%) - LeetHub

--- 1701-average-waiting-time/1701-average-waiting-time.java
+++ 1701-average-waiting-time/1701-average-waiting-time.java
@@ -1,11 +1,15 @@
 class Solution {
     public double averageWaitingTime(int[][] customers) {
-        long sum = 0;
-        int end = customers[0][0];
-        for (int[] customer : customers) {
-            sum += Math.max(end, customer[0]) + customer[1] - customer[0];
-            end = Math.max(end, customer[0]) + customer[1];
+        int n = customers.length;
+        long t = 0;
+        double sum = 0;
+        for (int i = 0; i < n; i++) {
+            if (t < customers[i][0]) {
+                t = customers[i][0];
+            }
+            t += customers[i][1];
+            sum += t - customers[i][0];
         }
-        return 1.0 * sum / customers.length;
+        return sum / n;
     }
 }
