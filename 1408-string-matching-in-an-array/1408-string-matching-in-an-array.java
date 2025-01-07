64d7bb8924d7771cdcf83315c243f1ec6aa0e231
Frank Zou <frankbearzou@gmail.com>
2025-01-06 19:42:09 -0500

    Time: 5 ms (41.61%), Space: 42.2 MB (54.32%) - LeetHub

--- 1408-string-matching-in-an-array/1408-string-matching-in-an-array.java
+++ 1408-string-matching-in-an-array/1408-string-matching-in-an-array.java
@@ -3,8 +3,10 @@ class Solution {
         List<String> res = new ArrayList<>();
         for (int i = 0; i < words.length; i++) {
             for (int j = 0; j < words.length; j++) {
-                if (words[i].length() < words[j].length() && words[j].indexOf(words[i]) != -1)
+                if (words[i].length() < words[j].length() && words[j].indexOf(words[i]) != -1) {
                     res.add(words[i]);
+                    break;
+                }
             }
         }
         return res;
