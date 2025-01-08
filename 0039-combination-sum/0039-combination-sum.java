f033f9adc6d5a5d0c839cbc8ce4eb5446ba4598c
Frank Zou <frankbearzou@gmail.com>
2025-01-08 11:02:17 -0500

    Time: 2 ms (83.38%), Space: 44.3 MB (91.76%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.java
+++ 0039-combination-sum/0039-combination-sum.java
@@ -1,30 +1,28 @@
 class Solution {
-    List<List<Integer>> res = new ArrayList<>();
-    List<Integer> path = new ArrayList<>();
+    List<List<Integer>> ans = new ArrayList<>();
     int[] candidates;
-    int n;
 
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
         this.candidates = candidates;
-        n = candidates.length;
-        dfs(0, target);
-        return res;
+        List<Integer> path = new ArrayList<>();
+        dfs(0, target, path);
+        return ans;
     }
 
-    void dfs(int start, int target) {
+    void dfs(int index, int target, List<Integer> path) {
+        if (index == candidates.length)
+            return;
+        if (target < 0)
+            return;
         if (target == 0) {
-            res.add(new ArrayList(path));
+            ans.add(new ArrayList<>(path));
             return;
         }
-        if (start == n)
-            return;
         // not choose
-        dfs(start + 1, target);
+        dfs(index + 1, target, path);
         // choose
-        if (candidates[start] <= target) {
-            path.add(candidates[start]);
-            dfs(start, target - candidates[start]);
-            path.removeLast();
-        }
+        path.add(candidates[index]);
+        dfs(index, target - candidates[index], path);
+        path.removeLast();
     }
 }
