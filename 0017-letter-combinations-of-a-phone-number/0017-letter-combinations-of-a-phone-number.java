6c67c592de1b9f17a619fcb5107598df74afeaa1
Frank Zou <frankbearzou@gmail.com>
2025-01-07 22:14:32 -0500

    Time: 5 ms (27.76%), Space: 41.9 MB (89.43%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
@@ -1,28 +1,33 @@
 class Solution {
-    int n;
-    List<String> ans = new ArrayList<>();
-    char[] digits;
-    char[] path;
-    String[] map = {"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
-
     public List<String> letterCombinations(String digits) {
-        n = digits.length();
-        if (n == 0)
+        List<String> ans = new ArrayList<>();
+        if (digits == null || digits.length() == 0)
             return ans;
-        this.digits = digits.toCharArray();
-        path = new char[n];
-        dfs(0);
+        String str = "";
+        Map<Character, String> map = new HashMap<>();
+        map.put('2', "abc");
+        map.put('3', "def");
+        map.put('4', "ghi");
+        map.put('5', "jkl");
+        map.put('6', "mno");
+        map.put('7', "pqrs");
+        map.put('8', "tuv");
+        map.put('9', "wxyz");
+        backtracking(digits, 0, map, str, ans);
         return ans;
     }
-
-    void dfs(int i) {
-        if (i == n) {
-            ans.add(new String(path));
+    
+    void backtracking(String digits, int start, Map<Character, String> map, String str, List<String> ans) {
+        if (str.length() == digits.length()) {
+            ans.add(str);
             return;
         }
-        for (char c : map[digits[i] - '0'].toCharArray()) {
-            path[i] = c;
-            dfs(i + 1);
+        for (int i = start; i < digits.length(); i++) {
+            char digit = digits.charAt(i);
+            String cur = map.get(digit);
+            for (char c : cur.toCharArray()) {
+                backtracking(digits, i + 1, map, str + c, ans);
+            }
         }
     }
 }
