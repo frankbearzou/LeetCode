af50506aaf382e5d046b149d46d487ca8f0ba3fc
Frank Zou <frankbearzou@gmail.com>
2025-01-09 13:21:11 -0500

    Time: 0 ms (100%), Space: 42.5 MB (84.94%) - LeetHub

--- 0078-subsets/0078-subsets.java
+++ 0078-subsets/0078-subsets.java
@@ -2,23 +2,20 @@ class Solution {
     List<List<Integer>> ans = new ArrayList<>();
     List<Integer> path = new ArrayList<>();
     int[] nums;
-    int n;
 
     public List<List<Integer>> subsets(int[] nums) {
-        n = nums.length;
         this.nums = nums;
         dfs(0);
         return ans;
     }
 
-    void dfs(int i) {
-        if (i == n) {
-            ans.add(new ArrayList(path));
-            return;
+    void dfs(int start) {
+        ans.add(new ArrayList<>(path));
+
+        for (int i = start; i < nums.length; i++) {
+            path.add(nums[i]);
+            dfs(i + 1);
+            path.removeLast();
         }
-        dfs(i + 1);
-        path.add(nums[i]);
-        dfs(i + 1);
-        path.removeLast();
     }
 }
