02392695a8658d1299a1586da639dfddbea8d3b8
Frank Zou <frankbearzou@gmail.com>
2025-01-09 11:47:33 -0500

    Time: 2 ms (88.76%), Space: 44.8 MB (80.07%) - LeetHub

--- 0051-n-queens/0051-n-queens.java
+++ 0051-n-queens/0051-n-queens.java
@@ -1,46 +1,50 @@
 class Solution {
+    int n;
+    char[][] board;
+    List<List<String>> ans = new ArrayList<>();
+
     public List<List<String>> solveNQueens(int n) {
-        List<List<String>> ans = new ArrayList<>();
-        char[][] board = new char[n][n];
+        this.n = n;
+        board = new char[n][n];
         for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                board[i][j] = '.';
-            }
+            Arrays.fill(board[i], '.');
         }
-        dfs(board, 0, ans);
+        dfs(0);
         return ans;
     }
 
-    void dfs(char[][] board, int row, List<List<String>> ans) {
-        if (row == board.length) {
+    void dfs(int row) {
+        if (row == n) {
             List<String> list = new ArrayList<>();
-            for (char[] arr : board) {
-                list.add(new String(arr));
+            for (int i = 0; i < n; i++) {
+                String s = new String(board[i]);
+                list.add(s);
             }
             ans.add(list);
+            return;
         }
-        for (int col = 0; col < board.length; col++) {
-            if (!isValid(board, row, col))
-                continue;
-            board[row][col] = 'Q';
-            dfs(board, row + 1, ans);
-            board[row][col] = '.';
+        for (int j = 0; j < n; j++) {
+            if (isValid(row, j)) {
+                board[row][j] = 'Q';
+                dfs(row + 1);
+                board[row][j] = '.';
+            }
         }
     }
 
-    boolean isValid(char[][] board, int row, int col) {
+    boolean isValid(int row, int col) {
         // column
         for (int i = 0; i < row; i++) {
             if (board[i][col] == 'Q')
                 return false;
         }
-        // upper left
+        // left upper
         for (int i = row - 1, j = col - 1; i >= 0 && j >= 0; i--, j--) {
             if (board[i][j] == 'Q')
                 return false;
         }
-        // upper right
-        for (int i = row - 1, j = col + 1; i >= 0 && j < board.length; i--, j++) {
+        //right upper
+        for (int i = row - 1, j = col + 1; i >= 0 && j < n; i--, j++) {
             if (board[i][j] == 'Q')
                 return false;
         }
