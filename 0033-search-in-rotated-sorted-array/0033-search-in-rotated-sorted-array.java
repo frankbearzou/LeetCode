8095b74999a2c4b15165a81ee78c13211740753a
Frank Zou <frankbearzou@gmail.com>
2025-01-14 10:57:49 -0500

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -5,13 +5,13 @@ class Solution {
             int m = l + (r - l) / 2;
             if (nums[m] == target)
                 return m;
-            if (nums[l] <= nums[m]) {
-                if (target >= nums[l] && target <= nums[m])
+            if (nums[l] < nums[m]) {
+                if (nums[l] <= target && target < nums[m])
                     r = m - 1;
                 else
                     l = m + 1;
             } else {
-                if (target >= nums[m] && target <= nums[r])
+                if (nums[m] < target && target <= nums[r])
                     l = m + 1;
                 else
                     r = m - 1;
