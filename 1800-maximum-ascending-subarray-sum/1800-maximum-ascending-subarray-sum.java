58529270bb4cd9228bcec24c23201be8c01cc050
Frank Zou <frankbearzou@gmail.com>
2025-02-03 23:08:09 -0500

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1800-maximum-ascending-subarray-sum/1800-maximum-ascending-subarray-sum.java
+++ 1800-maximum-ascending-subarray-sum/1800-maximum-ascending-subarray-sum.java
@@ -1,16 +1,16 @@
 class Solution {
     public int maxAscendingSum(int[] nums) {
-        int max = nums[0];
+        int ans = 0;
         int cur = nums[0];
         int n = nums.length;
         for (int i = 1; i < n; i++) {
             if (nums[i - 1] < nums[i]) {
                 cur += nums[i];
-                max = Math.max(max, cur);
             } else {
                 cur = nums[i];
             }
+            ans = Math.max(ans, cur);
         }
-        return max;
+        return ans;
     }
 }
