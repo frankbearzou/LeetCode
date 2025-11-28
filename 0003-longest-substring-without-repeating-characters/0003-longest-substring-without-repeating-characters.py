b33d18ec26118cf4480fe5df0fa945c391102746
Frank Zou <frankbearzou@gmail.com>
2025-11-28 14:45:01 -0500

    Time: 14 ms (83.74%), Space: 17.8 MB (52.31%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -4,9 +4,9 @@ class Solution:
         l = 0
         m = {}
         for r, c in enumerate(s):
-            if c in m:
-                l = m.get(c) + 1
-                del m[c]
+            while c in m:
+                del m[s[l]]
+                l += 1
             m[c] = r
             res = max(res, r - l + 1)
         return res
